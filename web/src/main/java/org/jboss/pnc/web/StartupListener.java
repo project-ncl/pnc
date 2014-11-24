30f1bad00c9fdb081eb149dcc1e6785f5c8992e8
Matej Lazar <matejonnet@gmail.com>
2014-11-24 14:28:15 +0100

    Start/stop logging.

--- web/src/main/java/org/jboss/pnc/web/StartupListener.java
+++ web/src/main/java/org/jboss/pnc/web/StartupListener.java
@@ -1,20 +1,25 @@
 package org.jboss.pnc.web;
 
+import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.logging.Logger;
 
 /**
  * Created by <a href="mailto:matejonnet@gmail.com">Matej Lazar</a> on 2014-11-23.
  */
 public class StartupListener implements ServletContextListener {
 
+    @Inject
+    Logger log;
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-
+        log.info("Application PNC started.");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-
+        log.info("Application PNC stopped.");
     }
 }
