2e870fc35e20fbf265b5e5f485a79c2cd11ffc10
Matej Lazar <matejonnet@gmail.com>
2015-01-01 20:51:01 +0100

    Fix core lifecycle bean.

--- pnc-core/src/main/java/org/jboss/pnc/core/Lifecycle.java
+++ pnc-core/src/main/java/org/jboss/pnc/core/Lifecycle.java
@@ -2,25 +2,20 @@ package org.jboss.pnc.core;
 
 import org.jboss.logging.Logger;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-
 
 /**
  * Not in use
  *
  * Created by <a href="mailto:matejonnet@gmail.com">Matej Lazar</a> on 2014-12-16.
  */
-@Deprecated
 public class Lifecycle {
 
     Logger log = Logger.getLogger(Lifecycle.class);
 
-    public Lifecycle(Logger log) {
+    public Lifecycle() {
     }
 
     public void start() {
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
         log.info("Core started.");
     }
 
