800c05c2d79617b6579a684dc17e3c3b1e8df2dc
Alex Creasy <acreasy@redhat.com>
2015-01-07 11:05:06 +0000

    0.3.0 UI bootstrapping: Initial modifications to yeoman angular generator

---
+++ pnc-ui/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pncUiApp
+ * @description
+ * # pncUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pncUiApp', []);
---
+++ pnc-ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pncUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pncUiApp
+ */
+angular.module('pncUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
