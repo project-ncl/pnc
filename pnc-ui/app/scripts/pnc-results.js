b930c404a4498deae77f0e2b7868da695baf1e9a
Andrea Vibelli <avibelli@redhat.com>
2015-01-16 12:23:48 +0100

    Refactoring of JavaScript after renaming of entities

--- pnc-ui/app/scripts/pnc-results.js
+++ pnc-ui/app/scripts/pnc-results.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
          method: 'GET',
          success: function (data) {
            $.each(data, function(entryIndex, entry){
-             if (entry.projectBuildConfigurationId === configurationId) {
+             if (entry.configurationIds === configurationId) {
                 filteredResults.push(entry);
              }
            });
