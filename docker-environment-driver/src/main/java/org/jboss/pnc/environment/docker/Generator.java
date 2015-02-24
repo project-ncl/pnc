5167b93c7701853537c2ff0f6be0d6f85d2519d1
Jakub Bartecek <jbartece@redhat.com>
2015-02-24 13:59:12 +0100

    DockerEnvironmentDriver: Improved generator + unit tests

--- docker-environment-driver/src/main/java/org/jboss/pnc/environment/docker/Generator.java
+++ docker-environment-driver/src/main/java/org/jboss/pnc/environment/docker/Generator.java
@@ -1,5 +1,6 @@
 package org.jboss.pnc.environment.docker;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 
 /**
@@ -19,48 +20,92 @@ public class Generator {
     private static final int MAX_SSH_PORT = 39999;
 
     private static final int MIN_SSH_PORT = 30000;
-
-    private static final String ID_PREFIX = "PNC-jenkins-container-";
-
-    private int suffix = 0;
     
-    private int latestJenkinsPort = MIN_JENKINS_PORT - 1;
-    
-    private int latestSshPort = MIN_SSH_PORT - 1;
+    private static final String DEFAULT_CONTAINER_ID = "PNC-jenkins-container-";
 
+    private String containerIdPrefix ;
+
+    private int latestContainerIdSuffix;
+
+    private int latestJenkinsPort;
+
+    private int latestSshPort;
+    
+    /**
+     * Process initialization of fields based on static constants
+     */
+    @PostConstruct
+    private void init() {
+        this.latestJenkinsPort = MIN_JENKINS_PORT - 1; 
+        this.latestSshPort = MIN_SSH_PORT - 1;
+        this.latestContainerIdSuffix = 0;
+        this.containerIdPrefix = DEFAULT_CONTAINER_ID;
+    }
 
     /**
      * @return New unique container id
      */
     public String generateContainerId() {
-        if (suffix > 10000)
-            suffix = 0;
-        suffix++;
+        if (latestContainerIdSuffix >= 10000)
+            latestContainerIdSuffix = 0;
+        latestContainerIdSuffix++;
 
-        return ID_PREFIX + suffix;
+        return containerIdPrefix + latestContainerIdSuffix;
     }
-    
+
     /**
      * 
      * @return New port for Jenkins
      */
-    public int generateJenkinsPort(){
-        if(latestJenkinsPort >= MAX_JENKINS_PORT)
+    public int generateJenkinsPort() {
+        if (latestJenkinsPort >= MAX_JENKINS_PORT)
             latestJenkinsPort = MIN_JENKINS_PORT - 1;
         latestJenkinsPort++;
-        
+
         return latestJenkinsPort;
     }
-    
+
     /**
      * 
      * @return New port for SSH
      */
-    public int generateSshPort(){
-        if(latestSshPort >= MAX_SSH_PORT)
+    public int generateSshPort() {
+        if (latestSshPort >= MAX_SSH_PORT)
             latestSshPort = MIN_SSH_PORT - 1;
         latestSshPort++;
-        
+
         return latestSshPort;
     }
+
+    /**
+     * USE THIS METHOD WITH CAUTION!
+     * It should be used only for test reason or when you are
+     * really sure, what you are doing.
+     * This method enables to influent generated values.
+     * 
+     * It is possible to directly set this values.
+     * 
+     * @param sshPort Next sshPort value (Max possible value 29999)
+     * @param jenkinsPort Next jenkinsPort value (Max value is 39999)
+     * @param containerIdPrefix Prefix of generated containerIds
+     */
+    public void forceNextValues(int sshPort, int jenkinsPort, String containerIdPrefix) {
+        this.latestJenkinsPort = jenkinsPort - 1;
+        this.latestSshPort = sshPort - 1;
+        this.containerIdPrefix = containerIdPrefix;
+    }
+    
+
+    /**
+     * USE THIS METHOD WITH CAUTION!
+     * It should be used only for test reason or when you are
+     * really sure, what you are doing.
+     * This method enables to influent generated values.
+     * 
+     * The state values are reinitialized to default values.
+     */
+    public void reInit() {
+        init();
+    }
+    
 }
