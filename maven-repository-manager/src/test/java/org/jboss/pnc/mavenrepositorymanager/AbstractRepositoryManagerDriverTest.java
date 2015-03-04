94765fa0429528fef270739a782fab11e7fdf9ef
Jakub Bartecek <jbartece@redhat.com>
2015-03-04 10:04:44 +0100

    Revert changes in AbstractRepositoryManagerDriverTest from commit 1514082288b089328638d1f79c20244d6b279d3d

--- maven-repository-manager/src/test/java/org/jboss/pnc/mavenrepositorymanager/AbstractRepositoryManagerDriverTest.java
+++ maven-repository-manager/src/test/java/org/jboss/pnc/mavenrepositorymanager/AbstractRepositoryManagerDriverTest.java
@@ -1,11 +1,12 @@
 package org.jboss.pnc.mavenrepositorymanager;
 
+import java.io.File;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 import org.commonjava.aprox.boot.BootStatus;
 import org.commonjava.aprox.test.fixture.core.CoreServerFixture;
 import org.jboss.pnc.common.Configuration;
+import org.jboss.pnc.common.json.ModuleConfigJson;
 import org.jboss.pnc.common.json.moduleconfig.MavenRepoDriverModuleConfig;
 import org.jboss.pnc.model.BuildConfiguration;
 import org.jboss.pnc.model.Product;
@@ -16,9 +17,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class AbstractRepositoryManagerDriverTest {
-    
-    private static final Logger log = Logger.getLogger(AbstractRepositoryManagerDriverTest.class.getName());
 
     @Rule
     public TemporaryFolder temp = new TemporaryFolder();
@@ -27,30 +28,58 @@ public class AbstractRepositoryManagerDriverTest {
     protected CoreServerFixture fixture;
     protected String url;
 
+    private String oldIni;
+
     @Before
     public void setup() throws Exception {
         fixture = new CoreServerFixture(temp);
-        fixture.start();
-        
+
+        Properties sysprops = System.getProperties();
+        oldIni = sysprops.getProperty(Configuration.CONFIG_SYSPROP);
+
         url = fixture.getUrl();
+        File configFile = temp.newFile("pnc-config.json");
+        ModuleConfigJson moduleConfigJson =  new ModuleConfigJson("pnc-config");
+        MavenRepoDriverModuleConfig mavenRepoDriverModuleConfig = 
+                new MavenRepoDriverModuleConfig(fixture.getUrl());
+        moduleConfigJson.addConfig(mavenRepoDriverModuleConfig);
+        
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(configFile, moduleConfigJson);
+
+        sysprops.setProperty(Configuration.CONFIG_SYSPROP, configFile.getAbsolutePath());
+        System.setProperties(sysprops);
+
+        fixture.start();
+
         if (!fixture.isStarted()) {
             final BootStatus status = fixture.getBootStatus();
             throw new IllegalStateException("server fixture failed to boot.", status.getError());
         }
 
-        log.info("Using base URL: " + url);
-        Configuration<MavenRepoDriverModuleConfig> config = new Configuration<>();
+        Properties props = new Properties();
+        props.setProperty("base.url", url);
+
+        System.out.println("Using base URL: " + url);
+
+        Configuration<MavenRepoDriverModuleConfig> config = new Configuration<MavenRepoDriverModuleConfig>();
         driver = new RepositoryManagerDriver(config);
     }
 
     @After
     public void teardown() throws Exception {
+        Properties sysprops = System.getProperties();
+        if (oldIni == null) {
+            sysprops.remove(Configuration.CONFIG_SYSPROP);
+        } else {
+            sysprops.setProperty(Configuration.CONFIG_SYSPROP, oldIni);
+        }
+        System.setProperties(sysprops);
+
         if (fixture != null) {
             fixture.stop();
         }
-    }
-
-    protected BuildConfiguration simpleBuildConfiguration() {
+    }    protected BuildConfiguration simpleBuildConfiguration() {
         Project project = new Project();
         project.setName("myproject");
 
