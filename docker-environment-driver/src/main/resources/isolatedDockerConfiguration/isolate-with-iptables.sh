88d2473cd9490042349bd1cee29c8e967d52c4db
Marek Novotný <hotmana76@gmail.com>
2015-03-05 14:45:35 +0100

    NCL-528 - changed passing allowed destinations

--- docker-environment-driver/src/main/resources/isolatedDockerConfiguration/isolate-with-iptables.sh
+++ docker-environment-driver/src/main/resources/isolatedDockerConfiguration/isolate-with-iptables.sh
@@ -2,7 +2,23 @@
 #
 # This script limit outband to defined $firewallAllowedDestinations IP addresses and its ports in $firewallAllowedPorts
 
+# basic preset for DNS discovery and accepting established connection
 iptables -I OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -I OUTPUT 1 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --match multiport --dport $firewallAllowedPorts -d $firewallAllowedDestinations -j ACCEPT
+
+# parses firewallAllowedDestinations environment variable
+# it should be in format IPAddress:port,[IPAddress:port, [ ... ]]
+OIFS=$IFS
+IFS=','
+for address in $firewallAllowedDestinations 
+do 
+ IFS=':'
+ destination=($address)
+ iptables -A OUTPUT -p tcp --dport ${destination[1]} -d ${destination[0]} -j ACCEPT
+done   
+IFS=$OIFS
+
 iptables -A OUTPUT -j REJECT
+
+# print out set the rules 
+iptables -L OUTPUT
