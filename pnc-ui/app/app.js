3eada123a04ab9462eec7e40d21d4cfaad80767e
pslegr <pslegr@redhat.com>
2015-03-20 09:11:45 +0100

    add Keycloak angular adapter into pnc-ui

--- pnc-ui/app/app.js
+++ pnc-ui/app/app.js
@@ -23,9 +23,96 @@
           'content@': {
             templateUrl: 'error.html'
           }
-        }
-      });
+        });
 
+        // Redirect any unmatched URLs to the error state.
+        $urlRouterProvider.otherwise('/error');
+      }
+    ]);
+  })();
+
+
+var auth = {};
+var logout = function(){
+    console.log('*** LOGOUT');
+    auth.loggedIn = false;
+    auth.authz = null;
+    window.location = auth.logoutUrl;
+};
+
+angular.element(document).ready(function ($http) {
+    var keycloakAuth = new Keycloak('keycloak.json');
+    auth.loggedIn = false;
+
+    keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+        auth.loggedIn = true;
+        auth.authz = keycloakAuth;
+        auth.logoutUrl = keycloakAuth.authServerUrl + '/realms/PNC.REDHAT.COM/tokens/logout?redirect_uri=/pnc-web/index.html';
+        module.factory('Auth', function() {
+            return auth;
+        });
+        angular.bootstrap(document, ['pnc']);
+    }).error(function () {
+            window.location.reload();
+        });
+
+});
+
+module.factory('authInterceptor', function($q, Auth) {
+    return {
+        request: function (config) {
+            var deferred = $q.defer();
+            if (Auth.authz.token) {
+                Auth.authz.updateToken(5).success(function() {
+                    config.headers = config.headers || {};
+                    config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+                    deferred.resolve(config);
+                }).error(function() {
+                        deferred.reject('Failed to refresh token');
+                    });
+            }
+            return deferred.promise;
+        }
+    };
+});
+
+
+
+
+module.config(function($httpProvider) {
+    $httpProvider.responseInterceptors.push('errorInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
+
+});
+
+module.factory('errorInterceptor', function($q) {
+    return function(promise) {
+        return promise.then(function(response) {
+            return response;
+        }, function(response) {
+            if (response.status === 401) {
+                console.log('session timeout?');
+                logout();
+            } else if (response.status === 403) {
+                alert('Forbidden');
+            } else if (response.status === 404) {
+                alert('Not found');
+            } else if (response.status) {
+                if (response.data && response.data.errorMessage) {
+                    alert(response.data.errorMessage);
+                } else {
+                    alert('An unexpected server error has occurred');
+                }
+            }
+            return $q.reject(response);
+        });
+    };
+});
+
+
+
+<<<<<<< HEAD
       // Redirect any unmatched URLs to the error state.
       $urlRouterProvider.otherwise('/error');
     }
@@ -46,3 +133,5 @@
   ]);
 
 })();
+=======
+>>>>>>> add Keycloak angular adapter into pnc-ui
