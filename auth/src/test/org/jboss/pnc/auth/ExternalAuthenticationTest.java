d8b41beb789b195008c121c015d13143d0132acb
pslegr <pslegr@redhat.com>
2015-04-01 10:32:14 +0200

    create separate auth module & Direct access grant impl

---
+++ auth/src/test/org/jboss/pnc/auth/ExternalAuthenticationTest.java
@@ -0,0 +1,28 @@
+package org.jboss.pnc.auth;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.keycloak.representations.AccessToken;
+
+public class ExternalAuthenticationTest {
+
+    @Test
+    public void testDAG() {
+        try {
+            InputStream is = this.getClass().getResourceAsStream("/keycloak.json");
+            ExternalAuthentication ea = new ExternalAuthentication(is);
+            AccessToken accessToken = ea.authenticate(System.getenv("PNC_EXT_OAUTH_USERNAME"), System.getenv("PNC_EXT_OAUTH_PASSWORD"));
+            AuthenticationProvider provider = new AuthenticationProvider(accessToken);
+            assertNotNull(provider);
+            assertNotNull(provider.getPrefferedUserName());
+            
+        } catch (IOException e) {
+            // do nothing
+        }
+    }
+
+}
