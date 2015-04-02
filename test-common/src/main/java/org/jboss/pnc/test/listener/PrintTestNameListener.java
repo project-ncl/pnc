a6a70f90e64f6638481fc86b1f09ebd61cc91a9b
Sebastian Laskawiec <slaskawi@redhat.com>
2015-04-02 11:06:44 +0200

    NCL-502 Remote tests shouldn't be executed 2 times

--- test-common/src/main/java/org/jboss/pnc/test/listener/PrintTestNameListener.java
+++ test-common/src/main/java/org/jboss/pnc/test/listener/PrintTestNameListener.java
@@ -2,15 +2,15 @@ package org.jboss.pnc.test.listener;
 
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunListener;
-
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PrintTestNameListener extends RunListener {
 
-    private Logger logger = Logger.getLogger(PrintTestNameListener.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PrintTestNameListener.class);
 
     @Override
     public void testStarted(Description description) {
-        logger.info("Running test: " + description.getClassName() + "#" + description.getMethodName());
+        logger.info("Running test: {}#{}", description.getClassName(), description.getMethodName());
     }
 }
