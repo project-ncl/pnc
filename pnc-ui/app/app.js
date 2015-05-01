6ec38d04731072d23432450faeb81c675c87f061
Alex Creasy <acreasy@redhat.com>
2015-05-01 11:43:08 +0100

    [NCL-782] Fixed UI errors caused by keycloak integration

--- pnc-ui/app/app.js
+++ pnc-ui/app/app.js
@@ -31,32 +31,79 @@
     'pnc.environment'
     ]);
 
-  app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-    function($stateProvider, $urlRouterProvider, $locationProvider) {
+  var auth = {};
 
-      $locationProvider.html5Mode(false).hashPrefix('!');
+  angular.element(document).ready(function () {
+    var keycloak = new Keycloak('keycloak.json');
+    auth.loggedIn = false;
 
-      $stateProvider.state('error', {
-        url: '/error',
-        views: {
-          'content@': {
-            templateUrl: 'error.html'
-          }
+    keycloak.init({ onLoad: 'login-required' }).success(function () {
+      auth.loggedIn = true;
+      auth.keycloak = keycloak;
+      auth.logout = function() {
+        auth.loggedIn = false;
+        auth.keycloak = null;
+        window.location = keycloak.authServerUrl + '/realms/PNC.REDHAT.COM/tokens/logout?redirect_uri=/pnc-web/index.html';
+      };
+      angular.bootstrap(document, ['pnc']);
+    }).error(function () {
+      window.location.reload();
+    });
+
+  });
+
+  app.factory('Auth', function () {
+    return auth;
+  });
+
+  app.factory('authInterceptor', function ($q, $log, Auth) {
+    return {
+      request: function (config) {
+        var deferred = $q.defer();
+
+        // if (config.url === 'rest/sender' || config.url === 'rest/registry/device/importer') {
+        //   return config;
+        // }
+
+        if (Auth.keycloak && Auth.keycloak.token) {
+          Auth.keycloak.updateToken(5).success(function () {
+            config.headers = config.headers || {};
+            config.headers.Authorization = 'Bearer ' + Auth.keycloak.token;
+
+            deferred.resolve(config);
+          }).error(function () {
+            deferred.reject('Failed to refresh token');
+          });
         }
-      });
-
-        // Redirect any unmatched URLs to the error state.
-        $urlRouterProvider.otherwise('/error');
+        return deferred.promise;
       }
-      ]);
+    };
+  });
 
-  app.run(['$rootScope', '$log', 'AuthService', 'ENV',
-    function($rootScope, $log, AuthService, ENV) {
+  app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+  });
 
-      if (ENV.name === 'prod') {
-        AuthService.login('keycloak.json');
+  app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(false).hashPrefix('!');
+
+    $stateProvider.state('error', {
+      url: '/error',
+      views: {
+        'content@': {
+          templateUrl: 'error.html'
+        }
       }
+    });
 
+      // Redirect any unmatched URLs to the error state.
+      $urlRouterProvider.otherwise('/error');
+    }
+  );
+
+  app.run(['$rootScope', '$log', 'ENV',
+    function($rootScope, $log, ENV) {
+      $log.debug('Environment=%O', ENV);
       // Handle errors with state changes.
       $rootScope.$on('$stateChangeError',
         function(event, toState, toParams, fromState, fromParams, error) {
