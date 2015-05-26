7b8998aab2d94df51d42bf4b5234874d575dfd38
pslegr <pslegr@redhat.com>
2015-05-26 14:21:28 +0200

    add simple OAuth client - more methods & refresh token

--- auth/src/main/java/org/jboss/pnc/auth/SimpleOAuthConnect.java
+++ auth/src/main/java/org/jboss/pnc/auth/SimpleOAuthConnect.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -15,9 +17,43 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 
 public class SimpleOAuthConnect {
+
+    public static String getAccessToken(String url, Map<String, String> urlParams) 
+            throws ClientProtocolException, IOException{
+        return connect(url, urlParams)[0];
+    }
+
+    public static String getRefreshToken(String url, Map<String, String> urlParams) 
+            throws ClientProtocolException, IOException{
+        return connect(url, urlParams)[1];
+    }
+    
+    public static String[] getTokens(String url, Map<String, String> urlParams) 
+            throws ClientProtocolException, IOException{
+        return connect(url, urlParams);
+    }
     
     public static String getAccessToken(String url, String clientId, String username, String password) 
             throws ClientProtocolException, IOException{
+        Map<String, String> urlParams = new HashMap<String, String>();
+        urlParams.put("client_id", clientId);
+        urlParams.put("username", username);
+        urlParams.put("password", password);
+        return connect(url, urlParams)[0];
+    }
+    
+    public static String getrefreshToken(String url, String clientId, String username, String password) 
+            throws ClientProtocolException, IOException{
+        Map<String, String> urlParams = new HashMap<String, String>();
+        urlParams.put("client_id", clientId);
+        urlParams.put("username", username);
+        urlParams.put("password", password);
+        return connect(url, urlParams)[1];
+    }
+    
+    private static String[] connect(String url, Map<String, String> urlParams) 
+            throws ClientProtocolException, IOException{
+        
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(url);
 
@@ -25,12 +61,14 @@ public class SimpleOAuthConnect {
         httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
 
         List <BasicNameValuePair> urlParameters = new ArrayList <BasicNameValuePair>();
-        urlParameters.add(new BasicNameValuePair("username", username));
-        urlParameters.add(new BasicNameValuePair("password", password));
-        urlParameters.add(new BasicNameValuePair("client_id", clientId));
+        for(String key : urlParams.keySet()) {
+            urlParameters.add(new BasicNameValuePair(key, urlParams.get(key)));
+        }
         httpPost.setEntity(new UrlEncodedFormEntity(urlParameters));
         CloseableHttpResponse response = httpclient.execute(httpPost);
 
+        String refreshToken = "";
+        String accessToken = "";
         try {
             BufferedReader rd = new BufferedReader(
                     new InputStreamReader(response.getEntity().getContent()));
@@ -42,11 +80,10 @@ public class SimpleOAuthConnect {
                     for (int i = 0; i < respContent.length; i++) {
                         String split = respContent[i];
                         if(split.contains("refresh_token")) {
-                            String refreshToken = split.split(":")[1].substring(1,split.split(":")[1].length() -1);
+                            refreshToken = split.split(":")[1].substring(1,split.split(":")[1].length() -1);
                         }
                         if(split.contains("access_token")) {
-                            String accessToken = split.split(":")[1].substring(1,split.split(":")[1].length() -1);
-                            return accessToken;
+                            accessToken = split.split(":")[1].substring(1,split.split(":")[1].length() -1);
                         }
                     }
                 }
@@ -55,9 +92,12 @@ public class SimpleOAuthConnect {
         } finally {
             response.close();
         }
-        return null;
-
+        return new String[] {accessToken,refreshToken};
+        
+        
     }
+    
+    
     
     public static void main(String[] args) {
         try {
