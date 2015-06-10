513b868500474d2469a2e3549867e5050a2725ed
Sebastian Laskawiec <slaskawi@redhat.com>
2015-06-10 14:32:06 +0200

    NCL-845 Return 409 if adding BuildConfiguration to BuildConfigurationSet the BuildConfiguration already exists in that set

---
+++ rest/src/test/java/org/jboss/pnc/rest/provider/BuildConfigurationSetProviderTest.java
@@ -0,0 +1,67 @@
+package org.jboss.pnc.rest.provider;
+
+import org.jboss.pnc.datastore.repositories.BuildConfigurationRepository;
+import org.jboss.pnc.datastore.repositories.BuildConfigurationSetRepository;
+import org.jboss.pnc.datastore.repositories.BuildRecordRepository;
+import org.jboss.pnc.model.BuildConfiguration;
+import org.jboss.pnc.model.BuildConfigurationSet;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class BuildConfigurationSetProviderTest {
+
+    @Test
+    public void addExistingBuildConfigurationToSet(){
+        BuildConfigurationSetRepository buildConfigurationSetRepository = mock(BuildConfigurationSetRepository.class);
+        BuildConfigurationRepository buildConfigurationRepository = mock(BuildConfigurationRepository.class);
+        BuildRecordRepository buildRecordRepository = mock(BuildRecordRepository.class);
+
+        BuildConfigurationSet testBCS = createBuildConfigurationSet(1);
+        addConfigsToSet(testBCS, createBuildConfiguration(1), createBuildConfiguration(2));
+
+        when(buildConfigurationSetRepository.findOne(1)).thenReturn(testBCS);
+        when(buildConfigurationRepository.findOne(2)).thenReturn(createBuildConfiguration(2));
+
+        BuildConfigurationSetProvider buildConfigurationSetProvider = new BuildConfigurationSetProvider(buildConfigurationSetRepository, buildRecordRepository, buildConfigurationRepository);
+        assertEquals(409, buildConfigurationSetProvider.addConfiguration(1,2).getStatus());
+    }
+
+    @Test
+    public void addNewBuildConfigurationToSet(){
+        BuildConfigurationSetRepository buildConfigurationSetRepository = mock(BuildConfigurationSetRepository.class);
+        BuildConfigurationRepository buildConfigurationRepository = mock(BuildConfigurationRepository.class);
+        BuildRecordRepository buildRecordRepository = mock(BuildRecordRepository.class);
+
+        BuildConfigurationSet testBCS = createBuildConfigurationSet(1);
+        addConfigsToSet(testBCS, createBuildConfiguration(1), createBuildConfiguration(2));
+
+        when(buildConfigurationSetRepository.findOne(1)).thenReturn(testBCS);
+        when(buildConfigurationRepository.findOne(3)).thenReturn(createBuildConfiguration(3));
+
+        BuildConfigurationSetProvider buildConfigurationSetProvider = new BuildConfigurationSetProvider(buildConfigurationSetRepository, buildRecordRepository, buildConfigurationRepository);
+        assertEquals(200, buildConfigurationSetProvider.addConfiguration(1,3).getStatus());
+    }
+
+    private BuildConfiguration createBuildConfiguration(int id){
+        BuildConfiguration retVal = new BuildConfiguration();
+        retVal.setId(id);
+        return retVal;
+    }
+
+    private BuildConfigurationSet createBuildConfigurationSet(int id){
+        BuildConfigurationSet retVal = new BuildConfigurationSet();
+        retVal.setId(id);
+        return retVal;
+    }
+
+    private void addConfigsToSet(BuildConfigurationSet bset, BuildConfiguration... bconfigs){
+        for (BuildConfiguration config : bconfigs){
+            bset.addBuildConfiguration(config);
+        }
+    }
+
+}
