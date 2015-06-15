da7dc0e5c6c69353f55c7a5690ede3a604cbecd8
Jakub Senko <jsenko@redhat.com>
2015-06-15 18:05:01 +0200

    Standalone page with a list of all running and finished builds.

--- ui/app/record/_record.js
+++ ui/app/record/_record.js
@@ -17,7 +17,7 @@
  */
 'use strict';
 
-(function() {
+(function () {
 
   var module = angular.module('pnc.record', [
     'ui.router',
@@ -29,87 +29,107 @@
   module.config([
     '$stateProvider',
     '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    function ($stateProvider, $urlRouterProvider) {
 
-    $stateProvider.state('record', {
-      abstract: true,
-      url: '/record',
-      views: {
-        'content@': {
-          templateUrl: 'common/templates/single-col.tmpl.html'
-          //templateUrl: 'common/templates/single-col-center.tmpl.html'
-        }
-      },
-      data: {
-        proxy: 'record.detail',
-      }
-    });
-
-    $urlRouterProvider.when('/record/:recordId', '/record/:recordId/info');
-
-    $stateProvider.state('record.detail', {
-      url: '/{recordId:int}',
-      templateUrl: 'record/views/record.detail.html',
-      data: {
-         displayName: '{{ recordDetail.name }}',
-      },
-      controller: 'RecordDetailController',
-      controllerAs: 'recordCtrl',
-      resolve: {
-        restClient: 'PncRestClient',
-        recordDetail: function(restClient, $stateParams) {
-          return restClient.Record.get({
-            recordId: $stateParams.recordId }).$promise;
+      $stateProvider.state('record', {
+        abstract: true,
+        url: '/record',
+        views: {
+          'content@': {
+            templateUrl: 'common/templates/single-col.tmpl.html'
+            //templateUrl: 'common/templates/single-col-center.tmpl.html'
+          }
         },
-        configurationDetail: function(restClient, recordDetail) {
-          return restClient.Configuration.get({
-            configurationId: recordDetail.buildConfigurationId }).$promise;
+        data: {
+          proxy: 'record.list',
+        }
+      });
+
+      $urlRouterProvider.when('/record/:recordId', '/record/:recordId/info');
+
+      $stateProvider.state('record.detail', {
+        url: '/{recordId:int}',
+        templateUrl: 'record/views/record.detail.html',
+        data: {
+          displayName: '{{ recordDetail.name }}',
         },
-        projectDetail: function(restClient, configurationDetail) {
-          return restClient.Project.get({
-            projectId: configurationDetail.projectId }).$promise;
+        controller: 'RecordDetailController',
+        controllerAs: 'recordCtrl',
+        resolve: {
+          restClient: 'PncRestClient',
+          recordDetail: function (restClient, $stateParams) {
+            return restClient.Record.get({
+              recordId: $stateParams.recordId
+            }).$promise;
+          },
+          configurationDetail: function (restClient, recordDetail) {
+            return restClient.Configuration.get({
+              configurationId: recordDetail.buildConfigurationId
+            }).$promise;
+          },
+          projectDetail: function (restClient, configurationDetail) {
+            return restClient.Project.get({
+              projectId: configurationDetail.projectId
+            }).$promise;
+          }
+        },
+      });
+
+      $stateProvider.state('record.detail.info', {
+        url: '/info',
+        templateUrl: 'record/views/record.detail.info.html',
+        data: {
+          displayName: '{{ recordDetail.id }}',
+        },
+        controller: 'RecordInfoController',
+        controllerAs: 'infoCtrl',
+      });
+
+      $stateProvider.state('record.detail.result', {
+        url: '/result',
+        controller: 'RecordResultController',
+        controllerAs: 'resultCtrl',
+        templateUrl: 'record/views/record.detail.result.html',
+        resolve: {
+          restClient: 'PncRestClient',
+          buildLog: function (restClient, recordDetail) {
+            return restClient.Record.getLog({
+              recordId: recordDetail.id
+            }).$promise;
+          }
         }
-      },
-    });
+      });
 
-    $stateProvider.state('record.detail.info', {
-      url: '/info',
-      templateUrl: 'record/views/record.detail.info.html',
-      data: {
-         displayName: '{{ recordDetail.id }}',
-      },
-      controller: 'RecordInfoController',
-      controllerAs: 'infoCtrl',
-    });
-
-    $stateProvider.state('record.detail.result', {
-      url: '/result',
-      controller: 'RecordResultController',
-      controllerAs: 'resultCtrl',
-      templateUrl: 'record/views/record.detail.result.html',
-      resolve: {
-        restClient: 'PncRestClient',
-        buildLog: function(restClient, recordDetail) {
-          return restClient.Record.getLog({
-            recordId: recordDetail.id}).$promise;
+      $stateProvider.state('record.detail.output', {
+        url: '/output',
+        controller: 'RecordOutputController',
+        controllerAs: 'outputCtrl',
+        templateUrl: 'record/views/record.detail.output.html',
+        resolve: {
+          restClient: 'PncRestClient',
+          artifacts: function (restClient, recordDetail) {
+            return restClient.Record.getArtifacts({
+              recordId: recordDetail.id
+            }).$promise;
+          }
         }
-      }
-    });
+      });
 
-    $stateProvider.state('record.detail.output', {
-      url: '/output',
-      controller: 'RecordOutputController',
-      controllerAs: 'outputCtrl',
-      templateUrl: 'record/views/record.detail.output.html',
-      resolve: {
-        restClient: 'PncRestClient',
-        artifacts: function(restClient, recordDetail) {
-          return restClient.Record.getArtifacts({
-            recordId: recordDetail.id}).$promise;
+
+      console.log('record.list');
+      $stateProvider.state('record.list', {
+        url: '',
+        templateUrl: 'record/views/record.list.html',
+        data: {
+          displayName: 'Records'
+        },
+        controller: 'RecordListController',
+        controllerAs: 'ctrl',
+        resolve: {
+          restClient: 'PncRestClient'
         }
-      }
-    });
+      });
 
-  }]);
+    }]);
 
 })();
--- ui/app/record/record-controllers.js
+++ ui/app/record/record-controllers.js
@@ -3,27 +3,27 @@
  * Copyright 2014 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 'use strict';
 
-(function() {
+(function () {
 
   var module = angular.module('pnc.record');
 
   module.controller('RecordDetailController', [
     'recordDetail', 'configurationDetail', 'projectDetail',
-    function(recordDetail, configurationDetail, projectDetail) {
+    function (recordDetail, configurationDetail, projectDetail) {
       this.record = recordDetail;
       this.configuration = configurationDetail;
       this.project = projectDetail;
@@ -31,22 +31,47 @@
   ]);
 
   module.controller('RecordInfoController', ['$log',
-    function($log) {
+    function ($log) {
       $log.debug('RecordInfoController');
     }
   ]);
 
   module.controller('RecordResultController', ['$log', 'buildLog',
-    function($log, buildLog) {
+    function ($log, buildLog) {
       $log.debug('RecordResultController >> buildLog: %O', buildLog);
       this.log = buildLog.payload;
     }
   ]);
 
   module.controller('RecordOutputController', ['$log', 'artifacts',
-    function($log, artifacts) {
+    function ($log, artifacts) {
       $log.debug('RecordOutputController >> artifacts: %O', artifacts);
       this.artifacts = artifacts;
+    }
+  ]);
+
+
+  module.controller('RecordListController', [
+    '$log',
+    '$scope',
+    'BuildProgressService',
+    'PncRestClient',
+    'BuildRecordNotifications',
+    function ($log, $scope, BuildProgressService, PncRestClient, BuildRecordNotifications) {
+      /* jshint unused: false */
+
+      BuildProgressService.track($scope, 'runningRecords', function () {
+        return PncRestClient.Running.query().$promise;
+      }, [
+        BuildProgressService.Tester.IN_PROGRESS()
+      ], BuildProgressService.BUILD_RECORD_UPDATER);
+
+
+      BuildProgressService.track($scope, 'finishedRecords', function () {
+        return PncRestClient.Record.query().$promise;
+      }, [
+        BuildProgressService.Tester.FINISHED()
+      ], BuildProgressService.BUILD_RECORD_UPDATER);
     }
   ]);
 
