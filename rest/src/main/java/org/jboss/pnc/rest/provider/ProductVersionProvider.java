dcf7cde5471c83d403804495b8f6ddaa31531736
Tom Hauser <thauser@redhat.com>
2015-06-26 16:32:17 -0400

    NCL-935 allow querying of product-versions using only a product version id. Allow querying of all product verisons. Tweak endpoint paths.

--- rest/src/main/java/org/jboss/pnc/rest/provider/ProductVersionProvider.java
+++ rest/src/main/java/org/jboss/pnc/rest/provider/ProductVersionProvider.java
@@ -71,7 +71,16 @@ public class ProductVersionProvider {
     public ProductVersionProvider() {
     }
 
-    public List<ProductVersionRest> getAll(int pageIndex, int pageSize, String sortingRsql, String query, Integer productId) {
+    public List<ProductVersionRest> getAll(int pageIndex, int pageSize, String sortingRsql, String query) {
+        Predicate<ProductVersion> rsqlPredicate = rsqlPredicateProducer.getPredicate(ProductVersion.class, query);
+        PageInfo pageInfo = pageInfoProducer.getPageInfo(pageIndex, pageSize);
+        SortInfo sortInfo = sortInfoProducer.getSortInfo(sortingRsql);
+        return nullableStreamOf(productVersionRepository.queryWithPredicates(pageInfo, sortInfo, rsqlPredicate))
+                .map(toRestModel())
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductVersionRest> getAllForProduct(int pageIndex, int pageSize, String sortingRsql, String query, Integer productId){
         Predicate<ProductVersion> rsqlPredicate = rsqlPredicateProducer.getPredicate(ProductVersion.class, query);
         PageInfo pageInfo = pageInfoProducer.getPageInfo(pageIndex, pageSize);
         SortInfo sortInfo = sortInfoProducer.getSortInfo(sortingRsql);
@@ -80,9 +89,8 @@ public class ProductVersionProvider {
                 .collect(Collectors.toList());
     }
 
-    public ProductVersionRest getSpecific(Integer productId, Integer productVersionId) {
-        List<ProductVersion> productVersions = productVersionRepository.queryWithPredicates(withProductId(productId),
-                withProductVersionId(productVersionId));
+    public ProductVersionRest getSpecific(Integer productVersionId) {
+        List<ProductVersion> productVersions = productVersionRepository.queryWithPredicates(withProductVersionId(productVersionId));
         if (productVersions.size() > 0) {
             return new ProductVersionRest(productVersions.get(0));
         }
