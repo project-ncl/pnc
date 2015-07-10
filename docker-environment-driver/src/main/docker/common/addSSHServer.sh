ff035d51eeeb9d6a1fd133a6a6e89f5951cb003c
Nick Cross <ncross@redhat.com>
2015-07-10 11:55:10 +0100

    Make SSH Server optional. Write BuildAgent logs to /tmp

---
+++ docker-environment-driver/src/main/docker/common/addSSHServer.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# JBoss, Home of Professional Open Source.
+# Copyright 2014 Red Hat, Inc., and individual contributors
+# as indicated by the @author tags.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This script determines whether to activate the SSH daemon or not
+
+if [[ -z "$openSSH" ]]; then
+  echo "\$openSSH not defined, not activating OpenSSH!"
+else
+  /usr/sbin/sshd -D
+fi
