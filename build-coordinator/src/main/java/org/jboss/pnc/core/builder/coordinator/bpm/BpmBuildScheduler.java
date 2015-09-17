ec2ed7bc0d6863180d6a01b8c881139bcd2f7474
Matej Lazar <matejonnet@gmail.com>
2015-09-17 20:24:44 +0200

    Add required parameters to BPM process start.

--- build-coordinator/src/main/java/org/jboss/pnc/core/builder/coordinator/bpm/BpmBuildScheduler.java
+++ build-coordinator/src/main/java/org/jboss/pnc/core/builder/coordinator/bpm/BpmBuildScheduler.java
@@ -22,9 +22,12 @@ import org.jboss.pnc.common.Configuration;
 import org.jboss.pnc.common.json.ConfigurationParseException;
 import org.jboss.pnc.common.json.moduleconfig.BpmModuleConfig;
 import org.jboss.pnc.common.json.moduleprovider.PncConfigProvider;
+import org.jboss.pnc.common.util.StringUtils;
 import org.jboss.pnc.core.builder.coordinator.BuildScheduler;
 import org.jboss.pnc.core.builder.coordinator.BuildTask;
+import org.jboss.pnc.core.content.ContentIdentityManager;
 import org.jboss.pnc.core.exception.CoreException;
+import org.jboss.pnc.model.BuildConfigurationAudited;
 import org.jboss.pnc.spi.BuildStatus;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.ProcessInstance;
@@ -88,7 +91,7 @@ public class BpmBuildScheduler implements BuildScheduler {
     @Override
     public void startBuilding(BuildTask buildTask, Consumer<BuildStatus> onComplete) throws CoreException {
 
-        ProcessInstance processInstance = startProcess(buildTask.getBuildSetTask().getId());
+        ProcessInstance processInstance = startProcess(buildTask, buildTask.getBuildSetTask().getId());
         logger.info("New component build process started with process instance id {}.", processInstance.getId());
         registerCompleteListener(buildTask.getId(), onComplete);
     }
@@ -98,15 +101,22 @@ public class BpmBuildScheduler implements BuildScheduler {
         bpmCompleteListener.subscribe(bpmListener);
     }
 
-    private ProcessInstance startProcess(Integer buildTaskSetId) {
+    private ProcessInstance startProcess(BuildTask buildTask, Integer buildTaskSetId) {
         RemoteRestRuntimeEngineFactory restSessionFactory = new RemoteRestRuntimeEngineFactory(deploymentId, instanceUrl, user, password);
 
         RemoteRuntimeEngine engine = restSessionFactory.newRuntimeEngine();
         KieSession kieSession = engine.getKieSession();
 
+        BuildConfigurationAudited configurationAudited = buildTask.getBuildConfigurationAudited();
+
         Map<String, Object> parameters = new HashMap<>();
-        parameters.put("build-task-set-id", buildTaskSetId);
+        parameters.put("buildTaskId",buildTask.getId() );
+        parameters.put("buildTaskSetId", buildTaskSetId);
+        parameters.put("buildConfigurationRevision", configurationAudited.getRev());
+        parameters.put("buildRecordSetIdsCSV", StringUtils.toCVS(buildTask.getBuildRecordSetIds()));
+        parameters.put("buildConfigSetRecordId", buildTask.getBuildConfigSetRecordId());
+        parameters.put("buildContentId", ContentIdentityManager.getBuildContentId(buildTask.getBuildConfiguration()));
+
         return kieSession.startProcess(processId, parameters);
     }
-
 }
