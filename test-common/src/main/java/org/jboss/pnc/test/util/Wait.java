8d2a7901eaf8b79364aba504c7d04ca3245fa3b9
Sebastian Laskawiec <slaskawi@redhat.com>
2015-09-24 15:47:47 +0200

    Reworked wait condition and increased timeouts

--- test-common/src/main/java/org/jboss/pnc/test/util/Wait.java
+++ test-common/src/main/java/org/jboss/pnc/test/util/Wait.java
@@ -34,16 +34,12 @@ public class Wait {
 
     public static void forCondition(Supplier<Boolean> evaluationSupplier, long timeout, TemporalUnit timeUnit, String failedMessage) throws InterruptedException, TimeoutException {
         LocalDateTime started = LocalDateTime.now();
-        while (true) {
+        do {
+            Thread.sleep(100);
             if (started.plus(timeout, timeUnit).isBefore(LocalDateTime.now())) {
                 throw new TimeoutException(failedMessage + " Reached timeout " + timeout + " " + timeUnit);
             }
-            if (evaluationSupplier.get()) {
-                break;
-            } else {
-                Thread.sleep(100);
-            }
-        }
+        } while(!evaluationSupplier.get());
     }
 
 
