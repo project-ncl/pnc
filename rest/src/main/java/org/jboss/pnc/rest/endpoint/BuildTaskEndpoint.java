1ec182fbb163577f66efb4d42748feb26c6343fc
Matej Lazar <matejonnet@gmail.com>
2015-10-08 09:47:17 +0200

    Accpect POST insted of GET to notify task compeltion.

--- rest/src/main/java/org/jboss/pnc/rest/endpoint/BuildTaskEndpoint.java
+++ rest/src/main/java/org/jboss/pnc/rest/endpoint/BuildTaskEndpoint.java
@@ -33,6 +33,7 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -67,7 +68,7 @@ public class BuildTaskEndpoint {
     @ApiResponses(value = {
             @ApiResponse(code = SUCCESS_CODE, message = SUCCESS_DESCRIPTION)
     })
-    @GET
+    @POST
     @Path("/{taskId}/completed")
     public Response buildTaskCompleted(
             @ApiParam(value = "Build task id", required = true) @PathParam("taskId") int taskId,
