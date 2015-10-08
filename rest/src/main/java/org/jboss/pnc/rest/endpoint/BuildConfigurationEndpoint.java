dc8e816db3cb01b2cd0ed30be25305c96342503d
Matej Lazar <matejonnet@gmail.com>
2015-10-08 08:50:10 +0200

    Fixing get running task.

--- rest/src/main/java/org/jboss/pnc/rest/endpoint/BuildConfigurationEndpoint.java
+++ rest/src/main/java/org/jboss/pnc/rest/endpoint/BuildConfigurationEndpoint.java
@@ -33,6 +33,7 @@ import org.jboss.pnc.rest.provider.BuildConfigurationProvider;
 import org.jboss.pnc.rest.provider.BuildRecordProvider;
 import org.jboss.pnc.rest.provider.ProductVersionProvider;
 import org.jboss.pnc.rest.restmodel.BuildConfigurationRest;
+import org.jboss.pnc.rest.restmodel.BuildRecordRest;
 import org.jboss.pnc.rest.restmodel.ProductVersionRest;
 import org.jboss.pnc.rest.restmodel.response.Singleton;
 import org.jboss.pnc.rest.restmodel.response.error.ErrorResponseRest;
@@ -350,9 +351,14 @@ public class BuildConfigurationEndpoint extends AbstractEndpoint<BuildConfigurat
                     callbackUrl);
 
             UriBuilder uriBuilder = UriBuilder.fromUri(uriInfo.getBaseUri()).path("/result/running/{id}");
-            int runningBuildId = buildExecutionTask.getId();
-            URI uri = uriBuilder.build(runningBuildId);
-            return Response.ok(uri).header("location", uri).entity(buildRecordProvider.getSpecificRunning(runningBuildId)).build();
+//            int runningBuildId = buildExecutionTask.getId();
+            URI uri = uriBuilder.build(buildTaskId);
+            BuildRecordRest specificRunning = buildRecordProvider.getSpecificRunning(buildTaskId); //TODO decouple reference to build coordinator
+
+            Response response = Response.ok(uri).header("location", uri).entity(specificRunning).build();
+            logger.debug("Returning response [{}]", response);
+            logger.debug("Returning response body [{}]", response.getEntity());
+            return response;
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return Response.serverError().entity("Other error: " + e.getMessage()).build();
