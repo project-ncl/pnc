72cfb378e59a6d4b0d48222d35d4b635ae78eb74
Alex Creasy <acreasy@redhat.com>
2016-02-26 18:07:52 +0000

    NCL-1572 - Re-implemented builds-panel directive to improve performance. This is a short term fix to mitigate, but not solve, the performance problems described in this issue.

---
+++ ui/app/configuration/directives/pnc-builds-panel/pncBuildsPanel.js
@@ -0,0 +1,66 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+(function () {
+
+  var module = angular.module('pnc.configuration');
+
+  /**
+   * @ngdoc directive
+   * @name pnc.configuration:pncBuildsPanel
+   * @restrict E
+   * @param {number=} configuration-id
+   * The id of the BuildConfiguration to display builds for.
+   * @description
+   * Displays a panel with recently completed builds of the given BuildConfiguration.
+   * @example
+      <pnc-builds-panel configuration-id="4"></pnc-builds-panel>
+   * @author Alex Creasy
+   */
+  module.directive('pncBuildsPanel', [
+    function () {
+
+      function PncBuildsPanelCtrl($log, $scope, BuildRecordDAO, eventTypes) {
+        $scope.page = BuildRecordDAO.getPagedByConfiguration({
+          configurationId: $scope.configurationId
+        });
+
+        function update(event, payload) {
+          $log.debug('PncBuildsPanelCtrl::update >> event = %O, payload = %O', event, payload);
+          if (payload.buildConfigurationId === $scope.configurationId) {
+            $scope.page.reload();
+          }
+        }
+
+        $scope.$on(eventTypes.BUILD_STARTED, update);
+        $scope.$on(eventTypes.BUILD_FINISHED, update);
+      }
+
+      return {
+        restrict: 'E',
+        templateUrl: 'configuration/directives/pnc-builds-panel/pnc-builds-panel.html',
+        scope: {
+          configurationId: '='
+        },
+        controller: PncBuildsPanelCtrl
+      };
+    }
+  ]);
+
+})();
