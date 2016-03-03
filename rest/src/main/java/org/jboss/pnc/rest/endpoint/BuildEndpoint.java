ab0a1c78760e43e9aa8b3c0f282954174a266c56
Andrea Vibelli <avibelli@redhat.com>
2016-03-03 08:11:42 +0100

    Added new endpoint to retrieve completed or running BuildRecords (deprecates /build-records/{id}/completed-or-running)

--- rest/src/main/java/org/jboss/pnc/rest/endpoint/BuildEndpoint.java
+++ rest/src/main/java/org/jboss/pnc/rest/endpoint/BuildEndpoint.java
@@ -45,12 +45,14 @@ import org.jboss.pnc.rest.provider.BuildRecordProvider;
 import org.jboss.pnc.rest.restmodel.BuildRecordRest;
 import org.jboss.pnc.rest.restmodel.response.error.ErrorResponseRest;
 import org.jboss.pnc.rest.swagger.response.BuildRecordPage;
+import org.jboss.pnc.rest.swagger.response.BuildRecordSingleton;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -58,6 +60,8 @@ import javax.ws.rs.core.Response;
 
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.INVALID_DESCRIPTION;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.INVLID_CODE;
+import static org.jboss.pnc.rest.configuration.SwaggerConstants.NOT_FOUND_CODE;
+import static org.jboss.pnc.rest.configuration.SwaggerConstants.NOT_FOUND_DESCRIPTION;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.NO_CONTENT_CODE;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.NO_CONTENT_DESCRIPTION;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_INDEX_DEFAULT_VALUE;
@@ -107,6 +111,24 @@ public class BuildEndpoint extends AbstractEndpoint<BuildRecord, BuildRecordRest
             @ApiParam(value = SORTING_DESCRIPTION) @QueryParam(SORTING_QUERY_PARAM) String sort,
             @ApiParam(value = QUERY_DESCRIPTION, required = false) @QueryParam(QUERY_QUERY_PARAM) String q) {
         return fromCollection(buildRecordProvider.getRunningAndArchivedBuildRecords(pageIndex, pageSize, q, sort));
+    }
+
+    @ApiOperation(value = "Gets a BuildRecord (active or archived)")
+    @ApiResponses(value = {
+            @ApiResponse(code = SUCCESS_CODE, message = SUCCESS_DESCRIPTION, response = BuildRecordSingleton.class),
+            @ApiResponse(code = NOT_FOUND_CODE, message = NOT_FOUND_DESCRIPTION, response = BuildRecordSingleton.class),
+            @ApiResponse(code = INVLID_CODE, message = INVALID_DESCRIPTION, response = ErrorResponseRest.class),
+            @ApiResponse(code = SERVER_ERROR_CODE, message = SERVER_ERROR_DESCRIPTION, response = ErrorResponseRest.class)
+    })
+    @GET
+    @Path("/{id}")
+    public Response getSpecific(@ApiParam(value = "BuildRecord id", required = true) @PathParam("id") Integer id) {
+
+        Response resp = super.getSpecific(id);
+        if (resp.getStatus() == Response.Status.NOT_FOUND.getStatusCode()) {
+          resp = fromSingleton(buildRecordProvider.getSpecificRunning(id));
+        }
+        return resp;
     }
 
 }
