7598a2285858fb57ee18e313c07dcc942ef7eebb
Matej Lazar <matejonnet@gmail.com>
2016-03-07 10:27:58 +0100

    Do not serialize privete fields to json.

--- rest-model/src/main/java/org/jboss/pnc/rest/utils/JsonOutputConverterMapper.java
+++ rest-model/src/main/java/org/jboss/pnc/rest/utils/JsonOutputConverterMapper.java
@@ -18,12 +18,10 @@
 
 package org.jboss.pnc.rest.utils;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import com.fasterxml.jackson.datatype.jdk7.Jdk7Module;
 import org.jboss.pnc.model.Artifact;
 
@@ -42,7 +40,6 @@ public class JsonOutputConverterMapper {
         mapper.registerModule(new Jdk7Module());
 
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.setVisibilityChecker(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
     }
 
     public static String apply(Object objectToBeConverted) {
