551716ee7a78ad6769dd3b07ffb2ea2c7454385c
Paul Gier <pgier@redhat.com>
2016-04-21 15:29:03 -0500

    [NCL-1897] Disable the generic resteasy exception mapper
    It is interfering with common built in exceptions produced by resteasy

--- rest/src/main/java/org/jboss/pnc/rest/configuration/AllOtherExceptionsMapper.java
+++ rest/src/main/java/org/jboss/pnc/rest/configuration/AllOtherExceptionsMapper.java
@@ -21,12 +21,17 @@ import org.jboss.pnc.rest.restmodel.response.error.ErrorResponseRest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import java.lang.invoke.MethodHandles;
 
-@Provider
+//TODO: This is temporarily disabled due to it being too general and catching all
+// exceptions.  For example, this prevents invalid rest URL requests from returning
+// the proper 404 error code.  Before this is re-enabled by uncommenting the @Provider line
+// the exception type which is caught needs to be made more specific than just "Exception"
+//@Provider
 public class AllOtherExceptionsMapper implements ExceptionMapper<Exception> {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -34,6 +39,6 @@ public class AllOtherExceptionsMapper implements ExceptionMapper<Exception> {
     @Override
     public Response toResponse(Exception e) {
         logger.error("An exception occurred when processing REST response", e);
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorResponseRest(e)).build();
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorResponseRest(e)).type(MediaType.APPLICATION_JSON).build();
     }
 }
