6271b7564447c1f5fb7321839a1c1bd3db912599
Martin Kelnar <mkelnar@redhat.com>
2016-05-24 10:28:57 +0200

    NCL-1893 Show built artifacts in a project

--- ui/app/report/report-controllers.js
+++ ui/app/report/report-controllers.js
@@ -341,6 +341,55 @@
   ]);
 
 
+  module.controller('BuiltArtifactsInProjectReportController', [
+    '$scope',
+    '$state',
+    '$log',
+    'ReportDAO',
+    function($scope, $state, $log, ReportDAO) {
+
+      var that = this;
+
+      that.afterSearch = false;
+      that.defaultSortKey = 'groupId';
+      that.defaultReverse = false;
+
+      that.isResultNotEmpty = function() {
+        return !_.isEmpty(that.reportResults);
+      };
+      
+      that.reset = function(form) {
+        that.reportResults = [];
+        that.afterSearch = false;
+      }; 
+ 
+      that.search = function(scmUrl, revision, pomPath, additionalRepos) {
+        ReportDAO.getBuiltArtifactsInProject(scmUrl, revision, pomPath, additionalRepos).then(function(result) {
+          that.reportResults = result;
+          that.sortKey = that.defaultSortKey;
+          that.reverse = that.defaultReverse;
+          that.afterSearch = true;
+
+          // Default sorting 
+          that.reportResults = _.chain(that.reportResults).sortBy(function(result){ return result[that.defaultSortKey]; }).value();
+          
+        }, function() {
+          // error 
+
+          that.reportResults = [];
+        });
+      };
+
+      that.sort = function(keyname){
+        that.sortKey = keyname;   
+        that.reverse = !that.reverse;
+      };
+
+    }
+
+  ]);
+
+
 
 
 })();
