ee4191ca54807d0b073635cb67837477a734ff1e
Paul Gier <pgier@redhat.com>
2016-05-27 16:22:49 -0500

    Use standard angularjs date parsing, fix warning about tooltip-html-unsafe
    Uses angular tooltip-template instead of the deprecated tooltip-html-unsafe

--- ui/app/product/directives/pncProductVersions/pncProductVersions.js
+++ ui/app/product/directives/pncProductVersions/pncProductVersions.js
@@ -38,49 +38,6 @@
 
           scope.page = ProductVersionDAO.getPagedByProduct({productId: scope.productId});
 
-          var formatDate = function(date) {
-            return date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate();
-          };
-
-          var convertFromTimestamp = function(mSec) {
-            var now = new Date();
-            return new Date(mSec + (now.getTimezoneOffset() * 60 * 1000) - (12 * 60 * 60 * 1000));
-          };
-
-          scope.getMilestoneTooltip = function(milestone) {
-            var sDate = '';
-            var prDate = '';
-            var rDate = '';
-            if (milestone.startingDate) {
-              sDate = formatDate(convertFromTimestamp(milestone.startingDate));
-            }
-            if (milestone.plannedReleaseDate) {
-              prDate = formatDate(convertFromTimestamp(milestone.plannedReleaseDate));
-            }
-            if (milestone.releaseDate) {
-              rDate = formatDate(convertFromTimestamp(milestone.releaseDate));
-            }
-            var milestoneTooltip = '<strong>' + milestone.version + '</strong>' +
-              '<br><br><strong>Phase: </strong> &lt;tbd&gt; <br>' +
-              '<strong>Starting date: </strong>' + sDate + '<br>' +
-              '<strong>Planned release date: </strong>' + prDate + '<br>' +
-              '<strong>Release date: </strong>' + rDate + '<br>';
-            return milestoneTooltip;
-          };
-
-          scope.getReleaseTooltip = function(release) {
-            var rDate = '';
-            if (release.releaseDate) {
-              rDate = formatDate(convertFromTimestamp(release.releaseDate));
-            }
-            var milestoneVersion = '';
-            var releaseTooltip = '<strong>' + release.version + '</strong>' +
-              '<br><br><strong>Phase: </strong> &lt;tbd&gt; <br>' +
-              '<strong>Release date: </strong>' + rDate + '<br>' +
-              '<strong>Released from Milestone: </strong>' + milestoneVersion + '<br>' +
-              '<strong>Support Level: </strong>' + release.supportLevel + '<br>';
-            return releaseTooltip;
-          };
         }
       };
     }
