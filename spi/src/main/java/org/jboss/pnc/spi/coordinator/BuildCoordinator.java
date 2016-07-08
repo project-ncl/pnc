d65358de2d594afe91d10cdfe7674084eccfdb61
Paul Gier <pgier@redhat.com>
2016-07-08 10:50:21 -0500

    Rename method params in BuildCoordinator to be more clear

--- spi/src/main/java/org/jboss/pnc/spi/coordinator/BuildCoordinator.java
+++ spi/src/main/java/org/jboss/pnc/spi/coordinator/BuildCoordinator.java
@@ -29,9 +29,9 @@ import java.util.List;
 
 public interface BuildCoordinator {
 
-    BuildTask build(BuildConfiguration buildConfiguration, User user, boolean rebuildAll) throws BuildConflictException;
+    BuildTask build(BuildConfiguration buildConfiguration, User user, boolean forceRebuild) throws BuildConflictException;
 
-    BuildSetTask build(BuildConfigurationSet buildConfigurationSet, User user, boolean rebuildAll) throws CoreException;
+    BuildSetTask build(BuildConfigurationSet buildConfigurationSet, User user, boolean forceRebuildAll) throws CoreException;
 
     List<BuildTask> getSubmittedBuildTasks();
 
