5ef6a9239734c76e92617930bea8163441669dbf
Daniel Soukhov <dsoukhov@unused-10-15-17-220.yyz.redhat.com>
2016-08-03 15:55:18 -0400

    NCL-2273
    Create/update milestones form checks for version uniqueness

---
+++ ui/app/milestone/directives/unqiue-version.js
@@ -0,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+(function () {
+
+    var module = angular.module('pnc.milestone');
+
+    module.directive('versionUnique',['$q','versionFactory',
+
+    function ($q, versionFactory) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                ngModel.$asyncValidators.unique = function (modelValue, viewValue) {
+                    var deferred = $q.defer(),
+                        milestoneVersion = modelValue || viewValue,
+                        productVersionId = attrs.productVersionId,
+                        productVersion = attrs.productVersion;
+                    if (milestoneVersion && productVersionId && productVersion) {
+                        versionFactory.checkUniqueValue(productVersionId, milestoneVersion, productVersion)
+                        .then(function (unique) {
+                            if (unique) {
+                                deferred.resolve();
+                            }
+                            else {
+                                deferred.reject();
+                            }
+                        });
+                    }
+                    else {
+                        deferred.resolve();
+                     }
+
+                    return deferred.promise;
+                };
+            }
+        };
+    }
+    ]);
+
+}());
---
+++ ui/app/milestone/factories/get-milestones.js
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+(function () {
+
+    var module = angular.module('pnc.milestone');
+
+    module.factory('versionFactory', ['$http',
+
+        function ($http) {
+            var restUrl = '../pnc-rest/rest/product-milestones/product-versions/',
+                factory = {};
+
+            factory.checkUniqueValue = function (productVersionId, milestoneVersion, productVersion) {
+                return $http.get(restUrl + productVersionId, {cache: true}).then(
+                    function (results) {
+                        for (var i =0; i < results.data.content.length; i++){
+                            var milestone = results.data.content[i];
+                            if(milestone.version === productVersion+'.'+milestoneVersion){
+                                return false;
+                            }
+                        }
+                        return true;
+                    });
+            };
+
+            return factory;
+        }
+    ]);
+
+}());
