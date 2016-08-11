e5e978cb048360e994f82ddc24f4477f03b4f0d2
Tom Hauser <thauser@redhat.com>
2016-08-11 15:10:37 -0400

    fixes problem with swagger annotations denoting a NoneType return instead of an AttributeSingleton

---
+++ rest/src/main/java/org/jboss/pnc/rest/swagger/response/AttributeSingleton.java
@@ -0,0 +1,11 @@
+package org.jboss.pnc.rest.swagger.response;
+
+import org.jboss.pnc.rest.restmodel.response.Singleton;
+
+import java.util.Map;
+
+/**
+ * Created by thauser on 8/11/16.
+ */
+public class AttributeSingleton extends Singleton<Map<String,String>> {
+}
