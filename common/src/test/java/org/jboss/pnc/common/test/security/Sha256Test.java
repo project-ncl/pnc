cb53b15f5227b7a31277aa3891095f355f77cdc9
Matej Lazar <matejonnet@gmail.com>
2016-09-20 13:30:13 +0200

    Fix licence headers.

--- common/src/test/java/org/jboss/pnc/common/test/security/Sha256Test.java
+++ common/src/test/java/org/jboss/pnc/common/test/security/Sha256Test.java
@@ -1,6 +1,6 @@
 /**
  * JBoss, Home of Professional Open Source.
- * Copyright 2016 Red Hat, Inc., and individual contributors
+ * Copyright 2014 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 package org.jboss.pnc.common.test.security;
 
