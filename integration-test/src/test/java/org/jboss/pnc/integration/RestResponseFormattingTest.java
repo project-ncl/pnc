3543562f5139480ad170b647c0acc26d3b376c10
Matej Lazar <matejonnet@gmail.com>
2017-04-24 15:25:38 +0200

    Report REST exceptions in JSON format.

---
+++ integration-test/src/test/java/org/jboss/pnc/integration/RestResponseFormattingTest.java
@@ -0,0 +1,107 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.integration;
+
+import com.jayway.restassured.http.ContentType;
+import org.hamcrest.core.IsEqual;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.pnc.AbstractTest;
+import org.jboss.pnc.integration.deployments.Deployments;
+import org.jboss.pnc.test.category.ContainerTest;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import java.lang.invoke.MethodHandles;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.jboss.pnc.integration.deployments.Deployments.addBuildExecutorMock;
+import static org.jboss.pnc.integration.env.IntegrationTestEnv.getHttpPort;
+
+/**
+ * @author <a href="mailto:matejonnet@gmail.com">Matej Lazar</a>
+ */
+@RunWith(Arquillian.class)
+@Category(ContainerTest.class)
+public class RestResponseFormattingTest extends AbstractTest {
+    public static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Deployment(testable = false)
+    public static EnterpriseArchive deploy() {
+        EnterpriseArchive enterpriseArchive = Deployments.baseEar();
+
+        WebArchive restWar = enterpriseArchive.getAsType(WebArchive.class, REST_WAR_PATH);
+        restWar.addAsWebInfResource("beans-use-mock-executor.xml", "beans.xml");
+
+        JavaArchive coordinatorJar = enterpriseArchive.getAsType(JavaArchive.class, AbstractTest.COORDINATOR_JAR);
+        coordinatorJar.addAsManifestResource("beans-use-mock-executor.xml", "beans.xml");
+
+        addBuildExecutorMock(enterpriseArchive);
+
+        logger.info(enterpriseArchive.toString(true));
+        logger.info(restWar.toString(true));
+
+        return enterpriseArchive;
+    }
+
+    @Test
+    public void shouldReturnErrorInJsonFormat() {
+        String response = given().header("Accept", "application/json").contentType(ContentType.JSON).port(getHttpPort()).expect()
+                .statusCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
+                .body("errorMessage", IsEqual.equalTo("Test exception."))
+                .when().get("/pnc-rest/rest/test/throw").asString();
+        logger.info(response);
+
+    }
+
+    @Test
+    public void shouldReturnInJsonAndStatus404() {
+        String response = given().header("Accept", "application/json").contentType(ContentType.JSON).port(getHttpPort()).expect()
+                .statusCode(Response.Status.NOT_FOUND.getStatusCode())
+                .body("errorType", IsEqual.equalTo("NotFoundException"))
+                .when().get("/pnc-rest/rest/test/does-not-exists").asString();
+        logger.info(response);
+    }
+
+    @Test
+    public void shouldReturnInJsonAndStatusNoContent() {
+        String response = given().header("Accept", "application/json").contentType(ContentType.JSON).port(getHttpPort()).expect()
+                .statusCode(Response.Status.NO_CONTENT.getStatusCode())
+                .when().get("/pnc-rest/rest/test/nocontent").asString();
+        logger.info(response);
+    }
+
+    @Test
+    @Ignore //TODO enable once we updated to EAP7. Test fails, the problem seems to be in the backend
+    public void shouldReturnStatusUnauthorizedAndHeader() {
+        String response = given().header("Accept", "application/json").contentType(ContentType.JSON).port(getHttpPort()).expect()
+                .statusCode(Response.Status.UNAUTHORIZED.getStatusCode())
+                .header("WWW-Authenticate", IsEqual.equalTo("Bearer realm=\"test\""))
+                .when().get("/pnc-rest/rest/test/unathorized").asString();
+        logger.info(response);
+    }
+
+}
--- rest/src/main/java/org/jboss/pnc/rest/configuration/AllOtherExceptionsMapper.java
+++ rest/src/main/java/org/jboss/pnc/rest/configuration/AllOtherExceptionsMapper.java
@@ -17,26 +17,67 @@
  */
 package org.jboss.pnc.rest.configuration;
 
-import org.jboss.pnc.rest.utils.ErrorResponse;
+import org.jboss.pnc.rest.restmodel.response.error.ErrorResponseRest;
+import org.jboss.resteasy.spi.Failure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 
-//TODO: This is temporarily disabled due to it being too general and catching all
-// exceptions.  For example, this prevents invalid rest URL requests from returning
-// the proper 404 error code.  Before this is re-enabled by uncommenting the @Provider line
-// the exception type which is caught needs to be made more specific than just "Exception"
-//@Provider
+/**
+ * Mapper that catches all exception and extracts the http status code from the JAXRS/RESTEASY runtime exception.
+ * Status code extraction is required for proper response error codes eg. 404.
+ */
+@Provider
 public class AllOtherExceptionsMapper implements ExceptionMapper<Exception> {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Override
     public Response toResponse(Exception e) {
-        logger.error("An exception occurred when processing REST response", e);
-        return ErrorResponse.toResponse(e);
+        int status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+        Response response = null;
+
+        if (e instanceof WebApplicationException) {
+            response = ((WebApplicationException) e).getResponse();
+            logger.debug("An exception occurred when processing REST response", e);
+        } else if (e instanceof Failure) { //Resteasy support
+            Failure failure = ((Failure)e);
+            if (failure.getErrorCode() > 0) {
+                status = failure.getErrorCode();
+            }
+            response = failure.getResponse();
+            logger.debug("An exception occurred when processing REST response", e);
+        } else {
+            logger.error("An exception occurred when processing REST response", e);
+        }
+
+        Response.ResponseBuilder builder;
+
+        if (response != null) {
+            builder = Response.status(response.getStatus());
+//            b.entity(response.getEntity());
+
+            //copy headers
+            for (String headerName : response.getMetadata().keySet()) {
+                List<Object> headerValues = response.getMetadata().get(headerName);
+                for (Object headerValue : headerValues) {
+                    builder.header(headerName, headerValue);
+                }
+            }
+        } else {
+            builder = Response.status(status);
+        }
+
+        builder.entity(new ErrorResponseRest(e)).type(MediaType.APPLICATION_JSON);
+
+        return builder.build();
+
     }
 }
