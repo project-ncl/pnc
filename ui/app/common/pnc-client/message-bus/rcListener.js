858db9841a1972a4fe7bd5bb6fd5883da51ca12a
Alex Creasy <acreasy@redhat.com>
2017-09-05 13:53:29 +0100

    NCL-3300 NCL-3298 Improve error / success handling and fix View BuildConfig button on success
    (cherry picked from commit 368d6297530a4ec2d2a26aaa5a44875d566ae1fd)
    Signed-off-by: Alex Creasy <acreasy@redhat.com>

--- ui/app/common/pnc-client/message-bus/rcListener.js
+++ ui/app/common/pnc-client/message-bus/rcListener.js
@@ -16,20 +16,20 @@
  * limitations under the License.
  */
 (function () {
-  'use strict';
-
-  angular.module('pnc.common.pnc-client.message-bus', [
-    'angular-websocket'
-  ]).run([
-    'messageBus',
-    function (messageBus) {
-
-      messageBus.registerListener('processProgressUpdateListener');
-      messageBus.registerListener('buildStatusListener');
-      messageBus.registerListener('bccListener');
-      messageBus.registerListener('rcListener');
-
-    }
-  ]);
-
-})();
+    'use strict';
+  
+    angular.module('pnc.common.pnc-client.message-bus').factory('rcListener', [
+      '$log',
+      '$rootScope',
+      'eventTypes',
+      function ($log, $rootScope, eventTypes) {
+        return function (message) {
+          if (message.eventType && message.eventType.startsWith('RC_')) {
+            $rootScope.$broadcast(eventTypes.RC_BPM_NOTIFICATION, message);
+          }
+        };
+      }
+    ]);
+  
+  })();
+  
