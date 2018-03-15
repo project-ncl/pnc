e4b8ac6f60fbb64f19abdf067171903cd20e908e
Jakub Bartecek <jbartece@redhat.com>
2018-03-15 15:19:52 +0100

    Revert "Remove unnecesary sleep in the test"
    This reverts commit 88e6bc8aeedce92b4730bbf0ba0388416ab4a58c.

--- bpm/src/test/java/org/jboss/pnc/bpm/test/BpmSchedulerSmokeTest.java
+++ bpm/src/test/java/org/jboss/pnc/bpm/test/BpmSchedulerSmokeTest.java
@@ -110,6 +110,7 @@ public class BpmSchedulerSmokeTest {
         bpmManager.startTask(task);
 
         // notify
+        Thread.sleep(500L);
         assertEquals(new Integer(1), task.getTaskId());
 
         BpmStringMapNotificationRest notification = mock(BpmStringMapNotificationRest.class);
