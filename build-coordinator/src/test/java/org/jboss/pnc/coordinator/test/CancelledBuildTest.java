c335b30a75919e5204fa6c02bc541f1529a2ee0b
Jan Michalov <jmichalo@redhat.com>
2018-07-19 14:12:16 +0200

    [NCL-3896] implement cancel of buildSetTask on backend in DefaultBuildCoordinator

--- build-coordinator/src/test/java/org/jboss/pnc/coordinator/test/CancelledBuildTest.java
+++ build-coordinator/src/test/java/org/jboss/pnc/coordinator/test/CancelledBuildTest.java
@@ -21,10 +21,12 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.pnc.mock.datastore.DatastoreMock;
 import org.jboss.pnc.mock.model.builders.TestProjectConfigurationBuilder;
+import org.jboss.pnc.model.BuildConfigurationSet;
 import org.jboss.pnc.model.BuildRecord;
 import org.jboss.pnc.model.BuildStatus;
 import org.jboss.pnc.spi.BuildCoordinationStatus;
 import org.jboss.pnc.spi.coordinator.BuildCoordinator;
+import org.jboss.pnc.spi.coordinator.BuildSetTask;
 import org.jboss.pnc.spi.coordinator.BuildTask;
 import org.jboss.pnc.spi.events.BuildCoordinationStatusChangedEvent;
 import org.jboss.pnc.spi.exception.CoreException;
@@ -101,5 +103,101 @@ public class CancelledBuildTest extends ProjectBuilder {
         assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILDING, buildTaskId);
         assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILD_COMPLETED, buildTaskId);
         assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.CANCELLED, buildTaskId);
+    }
+
+    @Test (timeout = 5_000)
+    public void cancelBuildingConfigSetTestCase() throws Exception {
+        //given
+        DatastoreMock datastoreMock = new DatastoreMock();
+        TestProjectConfigurationBuilder configurationBuilder = new TestProjectConfigurationBuilder(datastoreMock);
+
+        BuildCoordinator coordinator = buildCoordinatorFactory.createBuildCoordinator(datastoreMock).coordinator;
+
+        BuildConfigurationSet configurationSet = configurationBuilder.buildConfigurationSetForCancel(1);
+
+        List<BuildCoordinationStatusChangedEvent> receivedStatuses = new ArrayList<>();
+
+        Consumer<BuildCoordinationStatusChangedEvent> onStatusUpdate = (event) -> {
+            receivedStatuses.add(event);
+            if (event.getBuildConfigurationId().equals(2) && event.getNewStatus().equals(BuildCoordinationStatus.BUILDING)) {
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        Thread.sleep(250); //wait a bit for build execution to start
+                        //we need to get buildConfigSet id to cancel BuildGroup, it is not provided by event class directly, so we need to dit it up from buildTaskId that event provides
+                        coordinator.cancelSet(coordinator
+                                .getSubmittedBuildTasks().stream()
+                                .filter(t -> event.getBuildTaskId().equals(t.getId()))
+                                .findAny() //got BuildTask
+                                .get()
+                                .getBuildSetTask() //got BuildConfigSet
+                                .getId());
+                    } catch (CoreException | InterruptedException e) {
+                        log.error("Unable to cancel the build.", e);
+                        Assert.fail("Unable to cancel the build.");
+                    }
+                });
+            }
+        };
+
+        //when
+        BuildSetTask buildSetTask = buildProjects(configurationSet,coordinator,onStatusUpdate,2);
+
+        //expect
+        List<BuildRecord> buildRecords = datastoreMock.getBuildRecords();
+
+        Assert.assertEquals("Incorrect number of build records in datastore: " + buildRecords, 3, buildRecords.size());
+
+        for (BuildRecord buildRecord : buildRecords) {
+            Assert.assertNotNull(buildRecord.getSubmitTime());
+            switch (buildRecord.getBuildConfigurationId()) {
+                case 1:
+                    Assert.assertEquals(BuildStatus.CANCELLED, buildRecord.getStatus());
+                    continue;
+                case 2:
+                    Assert.assertEquals(BuildStatus.CANCELLED, buildRecord.getStatus());
+                    break;
+                case 3:
+                    Assert.assertEquals(BuildStatus.SUCCESS, buildRecord.getStatus());
+                    break;
+                default:
+                    Assert.fail("Invalid build configuration ID");
+                    break;
+            }
+            Assert.assertNotNull(buildRecord.getStartTime());
+            Assert.assertNotNull(buildRecord.getEndTime());
+        }
+
+
+
+
+
+        // 3 is independent, 2 is dependent on 3, 1 is dependent on 2
+        for (BuildTask buildTask : buildSetTask.getBuildTasks()) {
+            Integer buildTaskId = buildTask.getId();
+            switch (buildTask.getBuildConfiguration().getId()) {
+                case 1:
+                    //Building status is skipped (cancelled before it can start building)
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.WAITING_FOR_DEPENDENCIES, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILD_COMPLETED, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.CANCELLED, buildTaskId);
+                    break;
+                case 2:
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.WAITING_FOR_DEPENDENCIES, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.ENQUEUED, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILDING, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILD_COMPLETED, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.CANCELLED, buildTaskId);
+                    break;
+                case 3:
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.ENQUEUED, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILDING, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.BUILD_COMPLETED, buildTaskId);
+                    assertStatusUpdateReceived(receivedStatuses, BuildCoordinationStatus.DONE, buildTaskId);
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
