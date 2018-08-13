ef1a00614dc2469ea039927f290a38af7cfcd119
Jakub Bartecek <jbartece@redhat.com>
2018-08-13 16:43:53 +0200

    [NCL-3886]: Relax ProductMilestone restrictions on model to allow CD format

--- model/src/main/java/org/jboss/pnc/model/ProductMilestone.java
+++ model/src/main/java/org/jboss/pnc/model/ProductMilestone.java
@@ -66,7 +66,7 @@ public class ProductMilestone implements GenericEntity<Integer> {
      * Contains the milestone version string.  This consists of a major, minor, and micro
      * numeric version followed by an alphanumeric qualifier.  For example "1.0.0.ER1".
      */
-    @Pattern(message="The version should consist of three numeric parts and one alphanumeric qualifier each separated by a dot" , regexp="^[0-9]+\\.[0-9]+\\.[0-9]+\\.[\\w]+$")
+    @Pattern(message="The version should consist of three numeric parts and one alphanumeric qualifier each separated by a dot" , regexp="^[0-9]+\\.[0-9]+(\\.[0-9]+)?\\.[\\w]+$")
     @NotNull
     @Size(max=50)
     private String version;
