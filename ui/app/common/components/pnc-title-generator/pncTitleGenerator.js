4e5e5e67d654b65d4c249eafd2df0dee1fe5cab0
Alex Creasy <acreasy@redhat.com>
2018-09-11 14:44:26 +0100

    NCL-3920 Adds a directive for generating static page titles based on state definition item 'data.tiles'.

---
+++ ui/app/common/components/pnc-title-generator/pncTitleGenerator.js
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function () {
+  'use strict';
+
+  var TITLE_SUFFIX = ' | Project Newcastle';
+  var DEFAULT_TILE = 'Project Newcastle';
+
+  angular.module('pnc.common.components').directive('pncTitleGenerator', [
+    '$transitions',
+    function ($transitions) {
+      return {
+        restrict: 'A',
+        link: function(scope, elem) {
+          if (elem[0].tagName !== 'TITLE') {
+            throw new Error('pnc-title-generator directive can only be attached to the <title> tag');
+          }
+
+          function generateTitle(transition) {
+            var title = transition.to().data.title;
+
+            if (!angular.isString(title)) {
+              setTitle(DEFAULT_TILE);
+              return;
+            }
+
+            setTitle(title + TITLE_SUFFIX);
+          }
+
+          function setTitle(title) {
+            elem[0].textContent = title;
+          }
+
+          $transitions.onSuccess({}, generateTitle);
+        }
+      };
+    }
+  ]);
+
+})();
