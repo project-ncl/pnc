efca154b8a0b89d6bae0ddc0bda70b61f2d1a65a
Honza Brázdil <jbrazdil@redhat.com>
2018-10-10 21:05:19 +0200

    Add first bunch of enums

---
+++ constants/src/main/java/org/jboss/pnc/enums/BuildStatus.java
@@ -0,0 +1,86 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2018 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.enums;
+
+/**
+ * Status of a running or completed build.
+ *
+ * @author Jakub Bartecek &lt;jbartece@redhat.com&gt;
+ *
+ */
+public enum BuildStatus {
+    /**
+     * Build completed successfully
+     */
+    SUCCESS (true),
+
+    /**
+     * Build failed
+     */
+    FAILED,
+
+    /**
+     * Build completed with test failures
+     */
+    UNSTABLE (true),
+
+    /**
+     * Build currently running
+     */
+    BUILDING,
+
+    /**
+     * Build rejected due to conflict with another build, or failed dependency build
+     */
+    REJECTED,
+
+    /**
+     * User cancelled the build
+     */
+    CANCELLED,
+
+    /**
+     * A system error prevented the build from completing
+     */
+    SYSTEM_ERROR,
+
+    /**
+     * It is not known what the build status is at this time
+     */
+    UNKNOWN,
+
+    /**
+     * There have not been any builds of this configuration, essentially the same meaning
+     * as a null status
+     */
+    NONE;
+
+    private final boolean completedSuccessfully;
+
+    BuildStatus() {
+        this(false);
+    }
+
+    BuildStatus(boolean completedSuccessfully) {
+        this.completedSuccessfully = completedSuccessfully;
+    }
+
+    public boolean completedSuccessfully() {
+        return completedSuccessfully;
+    }
+}
---
+++ constants/src/main/java/org/jboss/pnc/enums/SystemImageType.java
@@ -0,0 +1,43 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2018 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.enums;
+
+/**
+ * Enum that represents the type of the build environment system image which will be used
+ * for the build.  The system image type indicates which build environment driver(s) is
+ * capable of initializing the environment (container, vm, etc) in which the build will
+ * run.
+ */
+public enum SystemImageType {
+
+    /** A Docker image will be used to run the build */
+    DOCKER_IMAGE,
+
+    /** A raw virtual machine image */
+    VIRTUAL_MACHINE_RAW,
+
+    /** A virtual machine image in the qcow2 format  */
+    VIRTUAL_MACHINE_QCOW2,
+
+    /** The local operating system will be used to run the build,  Note, that
+     *  this should not be used in a production environment because allows
+     *  for non reproducible builds if the local system changes.
+     */
+    LOCAL_WORKSPACE;
+
+}
