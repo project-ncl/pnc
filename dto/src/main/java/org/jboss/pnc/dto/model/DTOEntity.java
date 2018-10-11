8f81dda15eaed8c41774faaab607ed5a35cefc3d
Honza Brázdil <jbrazdil@redhat.com>
2018-10-11 14:09:10 +0200

    Add part of DTO entity Ref classes

--- dto/src/main/java/org/jboss/pnc/dto/model/DTOEntity.java
+++ dto/src/main/java/org/jboss/pnc/dto/model/DTOEntity.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.pnc.dto;
+package org.jboss.pnc.dto.model;
 
 /**
  * A base interface for all DTO entities
