d30d55c0be771565165bda81073ebc4132f038ee
Honza Brázdil <jbrazdil@redhat.com>
2018-11-14 16:51:39 +0100

    Update Build Config endpoint API

---
+++ dto/src/main/java/org/jboss/pnc/dto/requests/BuildConfigWithSCMRequest.java
@@ -0,0 +1,51 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2018 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.dto.requests;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.jboss.pnc.dto.BuildConfiguration;
+
+import javax.validation.Valid;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
+ */
+@Data
+@Builder(builderClassName = "Builder")
+public class BuildConfigWithSCMRequest {
+
+    @NotBlank
+    private final String scmUrl;
+
+    private final Boolean preBuildSyncEnabled;
+
+    @Valid // TODO this causes problems beacuase of @RefHasId RepositoryConfiguration repositoryConfiguration;
+    private final BuildConfiguration buildConfiguration;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static final class Builder {
+    }
+}
