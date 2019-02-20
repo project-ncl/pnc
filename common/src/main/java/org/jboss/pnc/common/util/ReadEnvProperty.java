3e2cb78b50593cf520ea182904b0ccc8c5955146
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-02-20 10:35:40 -0500

    Add retries for build env creation
    This commit adds retries for build env creation. The number of retries
    is configurable by setting 'creationPodRetry' value in the
    pnc-config.json
    
    We do the retry by identifying when the pod creation failed by checking
    its status periodically in monitors (or RunningTasks). The
    monitors are setup in `monitorInitialization`. Once the monitors are
    setup, they run in the background and `monitorInitialization` returns.
    
    If the pod creation failed, we throw an exception that is ultimately
    captured in `retryPod`. The latter will check the current number of
    retries allowed. If 0: we call the `onError` consumer to end the
    creation attempt. If greater than 0, we cancel and clear the existing
    monitors, destroy any existing build environment Openshift objects,
    recreate new build environment Openshift objects, and call
    `monitorInitialization` again, with the number of retries decremented.
    
    If the pod creation succeeded, we just call the `onComplete` consumer to
    signal that pod creation is done.
    
    This loop will continue until either the environment is created
    successfully, or there is a failure and the number of retries is 0.
    
    The side benefit of this commit is that instead of waiting for the pod to be
    in state 'Running', we now identify if the pod failed and take action
    based on this instead. This reduces the time we have to wait to take
    alternate actions.
    
    Note that in the case where pod creation fails with 'grpc connection
    unavailable', we cannot capture that error on the pod status (it remains
    on 'ContainerCreating' for some reason). However after <timeout> seconds
    the creation will timeout, and another retry will be attempted.
    
    Special thanks to @matejonnet for his review!

---
+++ common/src/main/java/org/jboss/pnc/common/util/ReadEnvProperty.java
@@ -0,0 +1,57 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReadEnvProperty {
+
+    private static final Logger log = LoggerFactory.getLogger(ReadEnvProperty.class);
+    /**
+     * If propertyName has no value (either specified in system property or environment property), then just return
+     * the default value. System property value has priority over environment property value.
+     *
+     * If value can't be parsed, just return the default value.
+     *
+     * @param propertyName property name to check the value
+     * @param defaultValue default value to use
+     *
+     * @return value from property, or default value
+     */
+    public int getIntValueFromPropertyOrDefault(String propertyName, int defaultValue) {
+
+        int value = defaultValue;
+
+        String valueEnv = System.getenv(propertyName);
+        String valueSys = System.getProperty(propertyName);
+
+        try {
+            if (valueSys != null) {
+                value = Integer.parseInt(valueSys);
+            } else if (valueEnv != null) {
+                value = Integer.parseInt(valueEnv);
+            }
+            return value;
+        } catch (NumberFormatException e) {
+            log.warn("Could not parse the '" + propertyName +
+                    "' system property. Using default value: " + defaultValue);
+            return value;
+        }
+    }
+}
