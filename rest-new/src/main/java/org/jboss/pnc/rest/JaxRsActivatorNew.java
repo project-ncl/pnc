53c0838efee1ea5c399a36cf31d59d0d70d50add
Jan Michalov <jmichalo@redhat.com>
2019-03-07 15:59:18 +0100

    [NCL-4351] RestEasy workaround

--- rest-new/src/main/java/org/jboss/pnc/rest/JaxRsActivatorNew.java
+++ rest-new/src/main/java/org/jboss/pnc/rest/JaxRsActivatorNew.java
@@ -17,6 +17,7 @@
  */
 package org.jboss.pnc.rest;
 
+import org.jboss.pnc.rest.endpoints.ArtifactEndpointImpl;
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
 import javax.ws.rs.ApplicationPath;
@@ -68,6 +69,7 @@ public class JaxRsActivatorNew extends Application {
     }
 
     private void addEndpoints(Set<Class<?>> resources) {
+        resources.add(ArtifactEndpointImpl.class);
     }
 
     private void addExceptionMappers(Set<Class<?>> resources) {
