960ed4a584c919980ee87256c152a98d475513a5
Jakub Bartecek <jbartece@redhat.com>
2019-03-15 13:55:40 +0100

    Fix license header

--- indy-repository-manager/src/test/java/org/jboss/pnc/indyrepositorymanager/ExtraDependencyRepositoriesTest.java
+++ indy-repository-manager/src/test/java/org/jboss/pnc/indyrepositorymanager/ExtraDependencyRepositoriesTest.java
@@ -1,14 +1,14 @@
 /**
  * JBoss, Home of Professional Open Source.
- * Copyright 2014 Red Hat, Inc., and individual contributors
+ * Copyright 2014-2019 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@
  * limitations under the License.
  */
 package org.jboss.pnc.indyrepositorymanager;
-
 
 import org.commonjava.indy.client.core.IndyClientException;
 import org.commonjava.indy.model.core.Group;
