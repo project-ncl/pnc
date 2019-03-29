aa4b7506fd60e07116b33cde9e3d209428535673
Jakub Bartecek <jbartece@redhat.com>
2019-03-29 14:29:54 +0100

    Fix code style

--- maven-repository-manager/src/main/java/org/jboss/pnc/mavenrepositorymanager/RepositoryManagerDriver.java
+++ maven-repository-manager/src/main/java/org/jboss/pnc/mavenrepositorymanager/RepositoryManagerDriver.java
@@ -282,7 +282,7 @@ public class RepositoryManagerDriver implements RepositoryManager {
 
             // add extra repositories removed from poms by the adjust process and set in BC by user
             List<ArtifactRepository> extraDependencyRepositories = extractExtraRepositoriesFromGenericParameters(genericParameters);
-            if(execution.getArtifactRepositories() != null) {
+            if (execution.getArtifactRepositories() != null) {
                 extraDependencyRepositories.addAll(execution.getArtifactRepositories());
             }
             addExtraConstituents(extraDependencyRepositories, id, buildContentId, indy, buildGroup);
@@ -294,8 +294,9 @@ public class RepositoryManagerDriver implements RepositoryManager {
 
     List<ArtifactRepository> extractExtraRepositoriesFromGenericParameters(Map<String, String> genericParameters) {
         String extraReposString = genericParameters.get(EXTRA_PUBLIC_REPOSITORIES_KEY);
-        if (extraReposString == null)
+        if (extraReposString == null) {
             return new ArrayList<>();
+        }
 
         return Arrays.stream(extraReposString.split("\n"))
                 .map((repoString) -> {
