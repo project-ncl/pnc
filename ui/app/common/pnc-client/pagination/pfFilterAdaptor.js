ab19a18dbaa03c3e2c69f869231edd1d5d47acf8
Alex Creasy <acreasy@redhat.com>
2019-05-14 16:23:56 +0100

    NCL-4705 Artifact list view filter doesn't work

--- ui/app/common/pnc-client/pagination/pfFilterAdaptor.js
+++ ui/app/common/pnc-client/pagination/pfFilterAdaptor.js
@@ -34,12 +34,15 @@
         var that = {};
 
         that.onFilterChange = function (filters) {
+          const filterDefinitions = this.fields;
           paginator.clearFilters();
-          filters.forEach(function (filter) {
+
+          filters.forEach(filter => {
+            const filterDef = filterDefinitions.find(fd => fd.id === filter.id);
             paginator.addFilter({
               field: filter.id,
               value: filter.value,
-              comparator: PF_FILTER_TYPES[filter.type]
+              comparator: PF_FILTER_TYPES[filterDef.filterType]
             });
           });
           paginator.apply();
