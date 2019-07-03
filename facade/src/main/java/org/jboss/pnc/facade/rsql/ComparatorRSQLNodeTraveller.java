f52cd03dd8fd87507b966208ac9d8c592c6d0729
Honza Brázdil <jbrazdil@redhat.com>
2019-07-03 16:50:31 +0200

    [NCL-4898] implement RSQLProducer DTO getComparator

---
+++ facade/src/main/java/org/jboss/pnc/facade/rsql/ComparatorRSQLNodeTraveller.java
@@ -0,0 +1,83 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2019 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.facade.rsql;
+
+import static org.jboss.pnc.facade.rsql.RSQLProducerImpl.DESC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.function.Function;
+
+import cz.jirutka.rsql.parser.ast.ComparisonNode;
+import cz.jirutka.rsql.parser.ast.LogicalNode;
+import java.lang.reflect.Method;
+import java.util.Comparator;
+import org.jboss.pnc.common.util.StringUtils;
+
+/**
+ *
+ * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
+ */
+class ComparatorRSQLNodeTraveller<DTO> extends RSQLNodeTraveller<Comparator<DTO>> {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Override
+    public Comparator<DTO> visit(LogicalNode logicalNode) {
+        return null;
+    }
+
+    @Override
+    public Comparator<DTO> visit(ComparisonNode node) {
+        logger.trace("Sorting direction - {}, arguments {}", node.getOperator(), node.getArguments());
+        Comparator<DTO> comparator = null;
+        for (String argument : node.getArguments()) {
+            Comparator<DTO> comp = Comparator.comparing(dto -> getProperty(dto, argument));
+            if(comparator == null){
+                comparator = comp;
+            }else{
+                comparator = comparator.thenComparing(comp);
+            }
+        }
+        if(comparator == null){
+            throw new IllegalArgumentException("No argument for RSQL comparsion found.");
+        }
+        if (node.getOperator().equals(DESC)) {
+            comparator = comparator.reversed();
+        }
+
+        return comparator;
+    }
+    
+    private Comparable getProperty(Object object, String argument) {
+        try {
+            String getter = "get" + StringUtils.firstCharToUpperCase(argument);
+            Method method = object.getClass().getMethod(getter);
+            Object obj = method.invoke(object);
+            if(obj instanceof Comparable){
+                return (Comparable) obj;
+            }else{
+                throw new IllegalArgumentException("Field " + argument + " is not comparable.");
+            }
+        } catch (ReflectiveOperationException | SecurityException ex) {
+                throw new RuntimeException("Could not access field " + argument + ": " + ex.getMessage(), ex);
+        }
+    }
+    
+}
