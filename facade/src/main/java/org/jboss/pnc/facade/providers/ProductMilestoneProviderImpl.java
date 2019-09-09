a035442175d31a955b58f244ab9d4c324057d237
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-09-09 16:32:18 -0400

    [NCL-5002] Add ProductMilestoneProviderTest
    This commit also triggers extra validation on updates

--- facade/src/main/java/org/jboss/pnc/facade/providers/ProductMilestoneProviderImpl.java
+++ facade/src/main/java/org/jboss/pnc/facade/providers/ProductMilestoneProviderImpl.java
@@ -92,6 +92,12 @@ public class ProductMilestoneProviderImpl
         validateDoesNotConflict(restEntity);
     }
 
+    @Override
+    protected void validateBeforeUpdating(String id, ProductMilestone restEntity) {
+        super.validateBeforeUpdating(restEntity.getId(), restEntity);
+        validateDoesNotConflict(restEntity);
+    }
+
     private void validateDoesNotConflict(ProductMilestone restEntity)
             throws ConflictedEntryException, InvalidEntityException {
         ValidationBuilder.validateObject(restEntity, WhenUpdating.class).validateConflict(() -> {
@@ -99,8 +105,14 @@ public class ProductMilestoneProviderImpl
                     withProductVersionIdAndVersion(Integer.valueOf(restEntity.getProductVersion().getId()), restEntity.getVersion())
             );
 
+            Integer restEntityId = null;
+
+            if (restEntity.getId() != null) {
+                restEntityId = Integer.valueOf(restEntity.getId());
+            }
+
             // don't validate against myself
-            if (milestoneFromDB != null && !milestoneFromDB.getId().equals(Integer.valueOf(restEntity.getId()))) {
+            if (milestoneFromDB != null && !milestoneFromDB.getId().equals(restEntityId)) {
                 return new ConflictedEntryValidator.ConflictedEntryValidationError(
                         milestoneFromDB.getId(),
                         org.jboss.pnc.model.ProductMilestone.class,
