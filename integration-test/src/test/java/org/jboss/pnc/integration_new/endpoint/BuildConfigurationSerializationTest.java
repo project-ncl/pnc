c4ee6b24abdac4bf96ff3e10bcfcc25912310909
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-09-10 13:21:36 -0400

    Move endpoint tests to 'endpoint' module

--- integration-test/src/test/java/org/jboss/pnc/integration_new/endpoint/BuildConfigurationSerializationTest.java
+++ integration-test/src/test/java/org/jboss/pnc/integration_new/endpoint/BuildConfigurationSerializationTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.pnc.integration_new;
+package org.jboss.pnc.integration_new.endpoint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.pnc.client.RemoteResourceException;
