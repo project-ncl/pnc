c8c4323eea9a3794aa1c01cd60fb67a90c32585d
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-09-10 13:21:36 -0400

    Move endpoint tests to 'endpoint' module

--- integration-test/src/test/java/org/jboss/pnc/integration_new/endpoint/SCMRepositoryEndpointTest.java
+++ integration-test/src/test/java/org/jboss/pnc/integration_new/endpoint/SCMRepositoryEndpointTest.java
@@ -1,4 +1,4 @@
-package org.jboss.pnc.integration_new;
+package org.jboss.pnc.integration_new.endpoint;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
