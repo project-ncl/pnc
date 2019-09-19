18e59dd360ea3b9b59abf76b8a748ec34baa4359
Alex Creasy <acreasy@redhat.com>
2019-09-19 16:01:57 +0100

    NCL-4200 fixes some links that were missed in initial refactor

--- ui/app/build-configs/build-configs.controllers.js
+++ ui/app/build-configs/build-configs.controllers.js
@@ -21,145 +21,16 @@
   var module = angular.module('pnc.build-configs');
 
   module.controller('ConfigurationListController', [
-    '$log',
-    '$state',
     'configurationList',
-    function($log, $state, configurationList) {
+    function(configurationList) {
       this.configurations = configurationList;
     }
   ]);
 
-  module.controller('ConfigurationDetailController', [
-    '$log',
-    '$state',
-    '$filter',
-    'pncNotify',
-    'ProductDAO',
-    'BuildConfigurationSetDAO',
-    'configurationDetail',
-    'environments',
-    'products',
-    'linkedProductVersions',
-    'dependencies',
-    'configurations',
-    'configurationSetList',
-    'linkedConfigurationSetList',
-    function($log, $state, $filter, pncNotify, ProductDAO, BuildConfigurationSetDAO,
-      configurationDetail, environments, products,
-      linkedProductVersions, dependencies, configurations, configurationSetList, linkedConfigurationSetList) {
-
-      var that = this;
-
-      that.configuration = configurationDetail;
-      that.environment = configurationDetail.environment;
-      that.environments = environments;
-      that.configurations = configurations;
-      that.configurationSetList = configurationSetList;
-      that.products = products;
-
-      that.productVersions = {
-        selected: linkedProductVersions
-      };
-
-      // Selection of dependencies
-      that.dependencies = {
-        selected: dependencies || []
-      };
-
-      // Selection of environments
-      that.environmentSelection = {
-        selected: [that.environment]
-      };
-
-      // Selection of ConfigurationSets
-      that.buildgroupconfigs = {
-        selected: _.clone(linkedConfigurationSetList)
-      };
-
-      // Update a build configuration after editting
-      that.update = function() {
-        $log.debug('Updating configuration: %O', that.configuration);
-
-        // The REST API takes integer Ids so we need to extract them from
-        // our collection of objects first and attach them to our data object
-        // for sending back to the server.
-        that.configuration.productVersionId =
-          getFirstId(that.productVersions.selected);
-        that.configuration.dependencyIds = that.dependencies.selected.map(function (d) { return d.id; });
-
-        that.configuration.environment.id = that.environment.id;
-
-        var added = _.difference(that.buildgroupconfigs.selected, linkedConfigurationSetList);
-        var removed = _.difference(linkedConfigurationSetList, that.buildgroupconfigs.selected);
-
-        that.configuration.$update().then(function() {
-
-          // Saving the BuildConfig link into the BuildGroupConfig for the added ones
-          _.each(added, function(buildgroupconfig) {
-            buildgroupconfig.buildConfigurationIds.push(that.configuration.id);
-            BuildConfigurationSetDAO.update(buildgroupconfig);
-          });
-
-          // Saving the BuildConfig link into the BuildGroupConfig for the added ones
-          _.each(removed, function(buildgroupconfig) {
-            var odds = _.reject(buildgroupconfig.buildConfigurationIds, function(thisBuildConfigId){ return thisBuildConfigId === that.configuration.id; });
-            buildgroupconfig.buildConfigurationIds = odds;
-            BuildConfigurationSetDAO.update(buildgroupconfig);
-          });
-
-          $state.go('projects.detail.build-configs.detail', {
-            configurationId: that.configuration.id,
-            projectId: that.configuration.project.id
-          }, {
-            reload: true
-          });
-        });
-      };
-
-      // Cloning a build configuration
-      that.clone = function() {
-        that.configuration.$clone().then(function(result) {
-          $state.go('projects.detail.build-configs.detail', {
-            configurationId: result.id,
-            projectId: result.project.id
-          }, {
-            reload: true
-          });
-        });
-      };
-
-      // Deleting a build configuration
-      that.delete = function() {
-        that.configuration.$delete().then(function() {
-          $state.go('projects.detail', {
-            projectId: that.configuration.project.id
-          }, {
-            reload: true,
-            inherit: false,
-            notify: true
-          });
-        });
-      };
-
-      that.cancel = function(form) {
-        if (form) {
-          $state.go('projects.detail.build-configs.detail', {
-            configurationId: that.configuration.id,
-            projectId: that.configuration.project.id
-          }, {
-            reload: true
-          });
-        }
-      };
-
-    }
-  ]);
-
   module.controller('ConfigurationSidebarController', [
-    '$log',
     '$stateParams',
     'configurationDetail',
-    function($log, $stateParams, configurationDetail) {
+    function($stateParams, configurationDetail) {
       this.buildConfigurationId = $stateParams.configurationId;
       this.buildConfiguration = configurationDetail;
       this.filterBy = {
@@ -169,10 +40,4 @@
     }
   ]);
 
-  function getFirstId(array) {
-    if (!array || array.length === 0) {
-      return null;
-    }
-    return array[0].id;
-  }
 })();
