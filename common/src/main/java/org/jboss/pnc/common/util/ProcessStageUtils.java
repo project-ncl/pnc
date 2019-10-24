2dbc5b2e8acec3ab65f38c16af439bb4f41c1b69
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-10-24 11:15:52 -0400

    [NCL-5267] Log PNC build related stages begin and end
    The process stages logged are:
    
    - Scheduling (when a build config is being broken down into build tasks)
    - Enqueued (build stayed in enqueued state)
    
    - The various stages in `BuildExecutionStatus`
      * NEW,
      * REPO_SETTING_UP,
      * BUILD_ENV_SETTING_UP,
      * BUILD_ENV_WAITING,
      * BUILD_ENV_SETUP_COMPLETE_SUCCESS,
      * BUILD_SETTING_UP,
      * BUILD_WAITING,
      * COLLECTING_RESULTS_FROM_BUILD_DRIVER,
      * COLLECTING_RESULTS_FROM_REPOSITORY_MANAGER,
      * REPOSITORY_MANAGER_SEALING_TRACKING_RECORD,
      * REPOSITORY_MANAGER_DOWNLOADING_TRACKING_REPORT,
      * REPOSITORY_MANAGER_PROCESSING_DEPENDENCIES,
      * REPOSITORY_MANAGER_PROCESSING_BUILT_ARTIFACTS,
      * REPOSITORY_MANAGER_REMOVE_BUILD_AGGREGATION_GROUP,
      * REPOSITORY_MANAGER_PROMOTION_BUILD_CONTENT_SET,
      * COLLECTING_RESULTS_FROM_REPOSITORY_MANAGER_COMPLETED_SUCCESS,
      * BUILD_COMPLETED_SUCCESS,
      * BUILD_ENV_DESTROYING,
      * BUILD_ENV_DESTROYED,
      * FINALIZING_EXECUTION,

---
+++ common/src/main/java/org/jboss/pnc/common/util/ProcessStageUtils.java
@@ -0,0 +1,60 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2019 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProcessStageUtils {
+
+    private static final Logger log = LoggerFactory.getLogger("org.jboss.pnc._userlog_.process-stage-update");
+    public static final String MDC_PROCESS_STAGE_NAME = "process_stage_name";
+    public static final String MDC_PROCESS_STAGE_STEP = "process_stage_step";
+
+    /**
+     * Log the process stage's begin and end stage
+     *
+     * Note that in the future this might be modified to send logs via Kafka
+     *
+     * @param processStage process stage name
+     */
+    public static void logProcessStage(String processStage, Step step) {
+
+        try (MDC.MDCCloseable a = MDC.putCloseable(MDC_PROCESS_STAGE_NAME, processStage);
+             MDC.MDCCloseable b = MDC.putCloseable(MDC_PROCESS_STAGE_STEP, step.toString())) {
+
+            log.info("{}: {} ", step, processStage);
+        }
+    }
+
+    public static void logProcessStageBegin(String processStage) {
+        logProcessStage(processStage, Step.BEGIN);
+    }
+
+    public static void logProcessStageEnd(String processStage) {
+        logProcessStage(processStage, Step.END);
+    }
+
+    public enum Step {
+        BEGIN, END
+    }
+}
