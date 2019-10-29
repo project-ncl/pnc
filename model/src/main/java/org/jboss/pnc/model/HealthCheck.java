4f7f3d18cf96b3541588143e84451f800cccf491
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-10-29 17:05:48 -0400

    [NCL-4581] Add health check endpoint for PNC
    Right now the health check done are:
    
    - check that we can read from database
    - check that we can write to database
    
    The endpoint will return 500 if *any* of the checks fail, otherwise 200.
    
    More checks can be added in the `HealthCheckProviderImpl` in the future.
    
    We decided to not use the Microprofile health since it is in technology
    preview for EAP 7.2.x. At the point of writing, we are using EAP 7.2.x
    for PNC.

---
+++ model/src/main/java/org/jboss/pnc/model/HealthCheck.java
@@ -0,0 +1,56 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2019 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.util.Date;
+
+@Entity
+public class HealthCheck implements GenericEntity<Integer> {
+
+    public static final String SEQUENCE_NAME = "healthcheck_id_seq";
+
+    @Id
+    @SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME, allocationSize = 1, initialValue = 100)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+    private Integer id;
+
+    private Date date;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
