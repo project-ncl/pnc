fcdfcdc63e01036d371fccc8f7a8b48d4dcc3de4
Matej Lazar <matejonnet@gmail.com>
2019-12-07 01:56:51 +0100

    Improve polling monitor and environment driver.
    For more readable code Use CompletableFuture instead of onComplete and onError parameters.

--- common/src/main/java/org/jboss/pnc/common/monitor/PullingMonitor.java
+++ common/src/main/java/org/jboss/pnc/common/monitor/PullingMonitor.java
@@ -19,10 +19,7 @@
 package org.jboss.pnc.common.monitor;
 
 import org.jboss.pnc.common.concurrent.MDCExecutors;
-import org.jboss.pnc.common.util.ObjectWrapper;
 import org.jboss.pnc.common.util.ReadEnvProperty;
-import org.jboss.pnc.common.util.TimeUtils;
-import org.jboss.util.collection.ConcurrentSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,7 +28,6 @@ import javax.enterprise.context.ApplicationScoped;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -58,15 +54,12 @@ public class PullingMonitor {
     private static final int DEFAULT_EXECUTOR_THREADPOOL_SIZE = 4;
 
     private ScheduledExecutorService executorService;
-    private ScheduledExecutorService timeOutVerifierService;
-
-    private ConcurrentSet<RunningTask> runningTasks;
+    private ScheduledExecutorServiceWithTimeout scheduledExecutor;
 
     private int timeout;
     private int checkInterval;
 
     public PullingMonitor() {
-
         ReadEnvProperty reader = new ReadEnvProperty();
 
         timeout = reader.getIntValueFromPropertyOrDefault(PULLING_MONITOR_TIMEOUT_KEY, DEFAULT_TIMEOUT);
@@ -74,95 +67,32 @@ public class PullingMonitor {
 
         int threadSize = reader.getIntValueFromPropertyOrDefault(PULLING_MONITOR_THREADPOOL_KEY, DEFAULT_EXECUTOR_THREADPOOL_SIZE);
 
-        runningTasks = new ConcurrentSet<>();
-        startTimeOutVerifierService();
         executorService = MDCExecutors.newScheduledThreadPool(threadSize);
-    }
-
-    public RunningTask monitor(Runnable onMonitorComplete, Consumer<Exception> onMonitorError, Supplier<Boolean> condition) {
-        return monitor(onMonitorComplete, onMonitorError, condition, checkInterval, timeout, DEFAULT_TIME_UNIT);
+        scheduledExecutor = new ScheduledExecutorServiceWithTimeout(executorService);
     }
 
     /**
      * Periodically checks the condition and calls onMonitorComplete when it returns true.
      * If timeout is reached onMonitorError is called.
      *
-     * @param onMonitorComplete
-     * @param onMonitorError
      * @param condition
-     * @param checkInterval
-     * @param timeout
-     * @param timeUnit Unit used for checkInterval and timeout
+     * @return CancellableCompletableFuture
      */
-    public RunningTask monitor(Runnable onMonitorComplete, Consumer<Exception> onMonitorError, Supplier<Boolean> condition, int checkInterval, int timeout, TimeUnit timeUnit) {
-
-        ObjectWrapper<RunningTask> runningTaskReference = new ObjectWrapper<>();
-        Runnable monitor = () -> {
-            RunningTask runningTask = runningTaskReference.get();
-
-            if (runningTask == null) {
-                /* There might be a situation where this runnable is called before runningTaskReference is set with the
-                 * running task since this runnable is scheduled to run before runningTaskReference is set. In that
-                 * case, we just skip this runnable and re-run on the next scheduled interval with the assumption that
-                 * runningTaskReference will be set before the next re-run
-                 */
-                log.debug("runningTask not set yet inside the 'monitor' runnable! Skipping!");
-                return;
-            }
-            try {
-                // Check if given condition is satisfied
-                if (condition.get()) {
-                    onMonitorComplete.run();
-                    runningTasks.remove(runningTask);
-                    runningTask.cancel();
-                }
-            } catch (Exception e) {
-                log.error("Exception in monitor runnable", e);
-                try {
-                    onMonitorError.accept(e);
-                } catch (Exception e1) {
-                    log.error("Error while notifying error.", e1);
-                }
-                runningTasks.remove(runningTask);
-                runningTask.cancel();
-            }
-        };
-        ScheduledFuture<?> future = executorService.scheduleWithFixedDelay(monitor, 0L, checkInterval, timeUnit);
-        Consumer<RunningTask> onTimeout = (runningTask) -> {
-            runningTasks.remove(runningTask);
-            onMonitorError.accept(new MonitorException("Service was not ready in: " + timeout + " " + timeUnit.toString()));
-        };
-        RunningTask runningTask = new RunningTask(future, timeout, TimeUtils.chronoUnit(timeUnit), onTimeout);
-        runningTasks.add(runningTask);
-        runningTaskReference.set(runningTask);
-
-        return runningTask;
+    public CancellableCompletableFuture<Void> monitor(Supplier<Boolean> condition) {
+        return monitor(condition, checkInterval, timeout, DEFAULT_TIME_UNIT);
     }
 
-    public void cancelRunningTask(RunningTask task) {
-        runningTasks.remove(task);
-        task.cancel();
+    public CancellableCompletableFuture<Void> monitor(Supplier<Boolean> condition, int checkInterval, int timeout, TimeUnit timeUnit) {
+        return scheduledExecutor.scheduleWithFixedDelayAndTimeout(condition, 0L, checkInterval, timeout, timeUnit);
     }
-
-
 
     public ScheduledFuture<?> timer(Runnable task, long delay, TimeUnit timeUnit) {
         return executorService.schedule(task, delay, timeUnit);
     }
 
-    private void startTimeOutVerifierService() {
-        Runnable terminateTimedOutTasks = () -> {
-            runningTasks.parallelStream()
-                    .forEach(runningTask -> runningTask.terminateIfTimedOut());
-        };
-        timeOutVerifierService = MDCExecutors.newScheduledThreadPool(1);
-        timeOutVerifierService.scheduleWithFixedDelay(terminateTimedOutTasks, 0L, 250, TimeUnit.MILLISECONDS);
-    }
-
     @PreDestroy
     public void destroy() {
         executorService.shutdownNow();
-        timeOutVerifierService.shutdownNow();
     }
 
 }
