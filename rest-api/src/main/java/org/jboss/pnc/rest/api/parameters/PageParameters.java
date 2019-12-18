622069e7421a129f667cd9f3b97638baf4ff20d3
Honza Brázdil <jbrazdil@redhat.com>
2019-12-18 19:15:47 +0100

    Introduce PaginationParameters superclass for PageParameters

--- rest-api/src/main/java/org/jboss/pnc/rest/api/parameters/PageParameters.java
+++ rest-api/src/main/java/org/jboss/pnc/rest/api/parameters/PageParameters.java
@@ -17,23 +17,11 @@
  */
 package org.jboss.pnc.rest.api.parameters;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-
-import static org.jboss.pnc.rest.configuration.Constants.MAX_PAGE_SIZE;
-import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_INDEX_DEFAULT_VALUE;
-import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_INDEX_DESCRIPTION;
-import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_INDEX_QUERY_PARAM;
-import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_SIZE_DEFAULT_VALUE;
-import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_SIZE_DESCRIPTION;
-import static org.jboss.pnc.rest.configuration.SwaggerConstants.PAGE_SIZE_QUERY_PARAM;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.QUERY_DESCRIPTION;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.QUERY_QUERY_PARAM;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.SORTING_DESCRIPTION;
 import static org.jboss.pnc.rest.configuration.SwaggerConstants.SORTING_QUERY_PARAM;
 
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.QueryParam;
 
 import io.swagger.v3.oas.annotations.Parameter;
@@ -44,20 +32,7 @@ import lombok.Data;
  * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
  */
 @Data
-public class PageParameters {
-
-    @Parameter(description = PAGE_INDEX_DESCRIPTION)
-    @QueryParam(PAGE_INDEX_QUERY_PARAM)
-    @DefaultValue(PAGE_INDEX_DEFAULT_VALUE)
-    @PositiveOrZero
-    private int pageIndex;
-
-    @Parameter(description = PAGE_SIZE_DESCRIPTION)
-    @QueryParam(PAGE_SIZE_QUERY_PARAM)
-    @DefaultValue(PAGE_SIZE_DEFAULT_VALUE)
-    @Positive
-    @Max(MAX_PAGE_SIZE)
-    private int pageSize;
+public class PageParameters extends PaginationParameters {
 
     @Parameter(description = SORTING_DESCRIPTION)
     @QueryParam(SORTING_QUERY_PARAM)
