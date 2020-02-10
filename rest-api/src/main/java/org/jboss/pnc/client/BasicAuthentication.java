1322d6c672a7c393ba6064e63ad01c8baeca23ad
Matej Lazar <matejonnet@gmail.com>
2020-02-11 00:06:24 +0100

    Client compatibility RestEasy 3 & 4.

---
+++ rest-api/src/main/java/org/jboss/pnc/client/BasicAuthentication.java
@@ -0,0 +1,42 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2019 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.client;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * @author <a href="mailto:matejonnet@gmail.com">Matej Lazar</a>
+ */
+public class BasicAuthentication implements ClientRequestFilter {
+
+    private final String authHeader;
+
+    public BasicAuthentication(String username, String password) {
+        String userPass = username + ":" + password;
+        authHeader = "Basic " + Base64.getEncoder().encodeToString(userPass.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public void filter(ClientRequestContext requestContext) {
+        requestContext.getHeaders().putSingle(HttpHeaders.AUTHORIZATION, authHeader);
+    }
+}
