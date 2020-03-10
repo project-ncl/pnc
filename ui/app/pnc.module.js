d58d9b405ae9d9539a0c6f27051c5e640d695141
Dennis Zhou <tozhou@redhat.com>
2020-03-10 16:53:06 +0000

    [NCL-5482] Implement WebSocket feature to update the maintenance status for 1.8 in real time

--- ui/app/pnc.module.js
+++ ui/app/pnc.module.js
@@ -139,6 +139,7 @@
       }]);
 
       messageBus.connect(restConfig.getPncNotificationsUrl());
+      messageBus.connect(restConfig.getPncGenericSettingsNotificationsUrl());
 
       if (authService.isAuthenticated()) {
         authService.getPncUser().then(function (result) {
