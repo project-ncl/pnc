b70052a9e5e1d794813c15a9ffa791b19fce3c19
michalovjan <michalovj2@gmail.com>
2020-03-17 13:09:36 +0100

    [NCL-5483] added predicates to WebSocket client and enhanced clients for rest-client

--- rest-client/src/main/java/org/jboss/pnc/restclient/AdvancedBuildConfigurationClient.java
+++ rest-client/src/main/java/org/jboss/pnc/restclient/AdvancedBuildConfigurationClient.java
@@ -1,3 +1,20 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2019 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.pnc.restclient;
 
 import static org.jboss.pnc.restclient.websocket.predicates.BuildChangedNotificationPredicates.withBuildCompleted;
@@ -14,7 +31,7 @@ import org.jboss.pnc.rest.api.parameters.BuildParameters;
 import org.jboss.pnc.restclient.websocket.VertxWebSocketClient;
 import org.jboss.pnc.restclient.websocket.WebSocketClient;
 
-public class AdvancedBuildConfigurationClient extends BuildConfigurationClient {
+public class AdvancedBuildConfigurationClient extends BuildConfigurationClient implements AutoCloseable{
 
     private WebSocketClient webSocketClient;
 
@@ -35,4 +52,8 @@ public class AdvancedBuildConfigurationClient extends BuildConfigurationClient {
         return waitForBuild(build.getId());
     }
 
+    @Override
+    public void close() throws Exception {
+        webSocketClient.disconnect().get();
+    }
 }
