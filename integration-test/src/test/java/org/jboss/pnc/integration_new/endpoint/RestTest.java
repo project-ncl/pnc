c9cf1127a14fafda78fb818d8ab0613aa4ef610c
Dominik Brazdil <dbrazdil@redhat.com>
2020-05-11 11:10:17 +0200

    [NCL-5700] Port RestTest integration tests

---
+++ integration-test/src/test/java/org/jboss/pnc/integration_new/endpoint/RestTest.java
@@ -0,0 +1,286 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2020 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.integration_new.endpoint;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.pnc.AbstractTest;
+import org.jboss.pnc.common.json.JsonUtils;
+import org.jboss.pnc.common.util.IoUtils;
+import org.jboss.pnc.dto.Product;
+import org.jboss.pnc.dto.Project;
+import org.jboss.pnc.integration.matchers.JsonMatcher;
+import org.jboss.pnc.integration_new.setup.Deployments;
+import org.jboss.pnc.test.category.ContainerTest;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.jboss.pnc.integration.env.IntegrationTestEnv.getHttpPort;
+
+/**
+ * @author <a href="mailto:dbrazdil@redhat.com">Dominik Brazdil</a>
+ *
+ * @see org.jboss.pnc.demo.data.DatabaseDataInitializer
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+@Category(ContainerTest.class)
+public class RestTest extends AbstractTest {
+
+    public static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private static final String REST_PATH = "/pnc-rest-new/rest-new/";
+    private static final String USER_REST_ENDPOINT = REST_PATH + "users/";
+    private static final String PRODUCT_REST_ENDPOINT = REST_PATH + "products/";
+    private static final String PRODUCT_VERSION_REST_ENDPOINT = REST_PATH + "product-versions/";
+    private static final String PROJECT_REST_ENDPOINT = REST_PATH + "projects/";
+    private static final String JSON_PATCH = "application/json-patch+json";
+
+    private static String productId;
+    private static String productVersionId;
+    private static String projectId;
+    private static String newProductId;
+    private static String newProjectId;
+
+    @Deployment
+    public static EnterpriseArchive deploy() {
+        return Deployments.testEar();
+    }
+
+    @Test
+    public void shouldGetAllProducts() {
+        given().headers(testHeaders)
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .get(PRODUCT_REST_ENDPOINT)
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute(FIRST_CONTENT_ID, value -> productId = value));
+    }
+
+    @Test
+    @InSequence(1)
+    public void shouldGetSpecificProduct() {
+        given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PRODUCT_REST_ENDPOINT + productId)
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute("id"));
+    }
+
+    @Test
+    @InSequence(2)
+    public void shouldGetAllProductsVersionsForSpecificProduct() {
+        given().headers(testHeaders)
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .get(PRODUCT_REST_ENDPOINT + productId + "/versions")
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute(FIRST_CONTENT_ID, value -> productVersionId = value));
+    }
+
+    @Test
+    @InSequence(3)
+    public void shouldGetSpecificProductsVersions() {
+        given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PRODUCT_VERSION_REST_ENDPOINT + productVersionId)
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute("id", value -> productVersionId = value));
+    }
+
+    @Test
+    @InSequence(4)
+    public void shouldGetAllProjects() {
+        given().headers(testHeaders)
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .get(PROJECT_REST_ENDPOINT)
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute(FIRST_CONTENT_ID, value -> projectId = value));
+    }
+
+    @Test
+    @InSequence(5)
+    public void shouldGetSpecificProject() {
+        given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PROJECT_REST_ENDPOINT + projectId)
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute("id"));
+    }
+
+    @Test
+    @InSequence(6)
+    public void shouldGetCurrentUser() {
+        given().headers(testHeaders)
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .get(USER_REST_ENDPOINT + "current")
+                .then()
+                .statusCode(200)
+                .body(JsonMatcher.containsJsonAttribute("id"));
+    }
+
+    @Test
+    @InSequence(7)
+    public void shouldCreateNewProduct() throws IOException {
+        String rawJson = IoUtils.readFileOrResource("product", "product.json", getClass().getClassLoader());
+
+        given().headers(testHeaders)
+                .body(rawJson)
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .post(PRODUCT_REST_ENDPOINT)
+                .then()
+                .statusCode(201)
+                .body(JsonMatcher.containsJsonAttribute("id", value -> newProductId = value));
+    }
+
+    @Test
+    @InSequence(8)
+    public void shouldUpdateProduct() throws Exception {
+
+        Product product = given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PRODUCT_REST_ENDPOINT + newProductId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(Product.class);
+
+        assertThat(product.getId()).isEqualTo(newProductId);
+
+        // from product.json
+        assertThat(product.getName()).isEqualTo("JBoss Enterprise Application Platform 6");
+
+        String newName = "JBoss Enterprise Application Platform 7";
+        product = product.toBuilder().name(newName).build();
+
+        given().headers(testHeaders)
+                .body(JsonUtils.toJson(product))
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .put(PRODUCT_REST_ENDPOINT + newProductId)
+                .then()
+                .statusCode(204);
+
+        // Reading updated resource
+        given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PRODUCT_REST_ENDPOINT + newProductId)
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(newProductId))
+                .body("name", equalTo(newName));
+    }
+
+    @Test
+    @InSequence(9)
+    public void shouldCreateNewProject() throws Exception {
+        String rawJson = IoUtils.readFileOrResource("project", "project.json", getClass().getClassLoader());
+
+        given().headers(testHeaders)
+                .body(rawJson)
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .post(PROJECT_REST_ENDPOINT)
+                .then()
+                .statusCode(201)
+                .body(JsonMatcher.containsJsonAttribute("id", value -> newProjectId = value));
+    }
+
+    @Test
+    @InSequence(10)
+    public void shouldUpdateProject() throws Exception {
+        Project project = given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PROJECT_REST_ENDPOINT + newProjectId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(Project.class);
+
+        assertThat(project.getId()).isEqualTo(newProjectId);
+        // from project.json
+        assertThat(project.getName()).isEqualTo("New Project");
+
+        String newName = "New even more awesome project";
+        project = project.toBuilder().name(newName).build();
+
+        given().headers(testHeaders)
+                .body(JsonUtils.toJson(project))
+                .contentType(ContentType.JSON)
+                .port(getHttpPort())
+                .when()
+                .put(PROJECT_REST_ENDPOINT + newProjectId)
+                .then()
+                .statusCode(204);
+
+        // Reading updated resource
+        given().headers(testHeaders)
+                .contentType(JSON_PATCH)
+                .port(getHttpPort())
+                .when()
+                .get(PROJECT_REST_ENDPOINT + newProjectId)
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(newProjectId))
+                .body("name", equalTo(newName));
+    }
+
+}
