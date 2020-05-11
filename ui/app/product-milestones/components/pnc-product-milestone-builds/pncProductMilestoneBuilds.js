19269c4d7290d91350025b501da26002228e27a0
Martin Kelnar <mkelnar@redhat.com>
2020-05-11 15:34:03 +0200

    NCL-4204 Refactor performed builds

--- ui/app/product-milestones/components/pnc-product-milestone-builds/pncProductMilestoneBuilds.js
+++ ui/app/product-milestones/components/pnc-product-milestone-builds/pncProductMilestoneBuilds.js
@@ -19,23 +19,80 @@
   'use strict';
 
   angular.module('pnc.product-milestones').component('pncProductMilestoneBuilds', {
-    binding: {
+    bindings: {
       productMilestone: '<',
       performedBuilds: '<'
     },
     templateUrl: 'product-milestones/components/pnc-product-milestone-builds/pnc-product-milestone-builds.html',
-    controller: [Controller]
+    controller: ['SortHelper', 'filteringPaginator', Controller]
   });
 
-  function Controller() {
+  function Controller(SortHelper, filteringPaginator) {
     const $ctrl = this;
+    const PAGE_NAME = 'buildsList';
 
     // -- Controller API --
     $ctrl.displayFields = ['status', 'id', 'configurationName', 'endTime'];
 
+    $ctrl.performedBuildsFilteringFields = [{
+      id: 'user.username',
+      title: 'Username',
+      placeholder: 'Filter by Username',
+      filterType: 'text'
+    }, {
+      id: 'buildConfig.name',
+      title: 'Build Config name',
+      placeholder: 'Filter by Build Config name',
+      filterType: 'text'
+    }, {
+      id: 'status',
+      title: 'Status',
+      placeholder: 'Filter by Status',
+      filterType: 'select',
+      filterValues: [
+        'SUCCESS',
+        'REJECTED',
+        'FAILED',
+        'CANCELLED',
+        'BUILDING',
+        'NO_REBUILD_REQUIRED',
+        'SYSTEM_ERROR'
+      ]
+    }, {
+      id: 'temporaryBuild',
+      title: 'Temporary Build',
+      placeholder: 'Filter by Temporary Build',
+      filterType: 'select',
+      filterValues: [
+        'FALSE',
+        'TRUE'
+      ]
+    }];
+
+
+    $ctrl.performedBuildsSortingFields = [{
+      id: 'status',
+      title: 'Status',
+    }, {
+      id: 'startTime',
+      title: 'Start Time',
+    }, {
+      id: 'submitTime',
+      title: 'Submit Time',
+    }, {
+      id: 'endTime',
+      title: 'End Time',
+    }, {
+      id: 'user.username',
+      title: 'Username',
+    }];
+
     // --------------------
 
-    $ctrl.$onInit = () => {};
+    $ctrl.$onInit = () => {
+      $ctrl.performedBuildsFilteringPage = filteringPaginator($ctrl.performedBuilds);
+      $ctrl.performedBuildsSortingConfigs = SortHelper.getSortConfig(PAGE_NAME);
+    };
 
   }
 
