dfd38b63f0c3235cc25ff221a35052aab4c2a8d4
Dustin Kut Moy Cheung <dcheung@redhat.com>
2020-06-26 10:13:25 -0400

    [NCLSUP-82] Print problematic build record

--- datastore/src/main/java/org/jboss/pnc/datastore/DefaultDatastore.java
+++ datastore/src/main/java/org/jboss/pnc/datastore/DefaultDatastore.java
@@ -457,7 +457,7 @@ public class DefaultDatastore implements Datastore {
     private boolean hasNewerVersion(BuildRecord buildRecord, boolean temporaryBuild) {
         BuildRecord latestSuccessfulBuildRecord = buildRecordRepository.getLatestSuccessfulBuildRecord(buildRecord.getBuildConfigurationAuditedIdRev(), temporaryBuild);
         if (latestSuccessfulBuildRecord == null) {
-            logger.error("Something went wrong, the buildRecord should be successful (ot this lattest or the BuildRecord that produced artifacts.).");
+            logger.error("Something went wrong, the buildRecord {} should be successful (to this latest or the BuildRecord that produced artifacts.).", buildRecord.getId());
         }
         return !buildRecord.getId().equals(latestSuccessfulBuildRecord.getId());
     }
