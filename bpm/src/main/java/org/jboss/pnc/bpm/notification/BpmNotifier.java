c4ab9cadf152ddbee94b0b55d5700fb8493bc314
Matej Lazar <matejonnet@gmail.com>
2020-09-20 23:00:45 +0200

    Send executor response as json in RHPAM callback.

--- bpm/src/main/java/org/jboss/pnc/bpm/notification/BpmNotifier.java
+++ bpm/src/main/java/org/jboss/pnc/bpm/notification/BpmNotifier.java
@@ -26,15 +26,16 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.jboss.pnc.bpm.model.BuildResultRest;
 import org.jboss.pnc.bpm.model.mapper.BuildResultMapper;
 import org.jboss.pnc.common.Configuration;
 import org.jboss.pnc.common.json.ConfigurationParseException;
 import org.jboss.pnc.common.json.moduleconfig.BpmModuleConfig;
 import org.jboss.pnc.common.json.moduleprovider.PncConfigProvider;
 import org.jboss.pnc.common.util.HttpUtils;
-import org.jboss.pnc.bpm.model.BuildResultRest;
 import org.jboss.pnc.spi.BuildResult;
 import org.jboss.pnc.spi.executor.BuildExecutionConfiguration;
 import org.slf4j.Logger;
@@ -90,34 +91,41 @@ public class BpmNotifier {
             errMessage = "Cannot construct rest result: " + e.getMessage();
         }
 
-        HttpPost request = new HttpPost(uri);
-
-        List<NameValuePair> parameters = new ArrayList<>();
-        parameters.add(
-                new BasicNameValuePair(
-                        "event",
-                        buildResultRest != null ? buildResultRest.toFullLogString()
-                                : "{\"error\", \"" + errMessage + "\"}"));
-
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(parameters, StandardCharsets.UTF_8);
-        if (entity != null) {
-            request.setEntity(entity);
-            log.debug("Response entity set to the post request for uri: " + uri);
-        } else {
-            log.error("Missing response entity to post to: " + uri);
-        }
-
+        boolean isNewBpmProcess = false;
         if (buildResult.getBuildExecutionConfiguration().isPresent()) {
             BuildExecutionConfiguration buildExecutionConfiguration = buildResult.getBuildExecutionConfiguration()
                     .get();
             if (buildExecutionConfiguration.getGenericParameters()
                     .getOrDefault(GENERIC_PARAMETER_KEY, "")
                     .equals(RHPAM)) {
-                configureRequestNewBPM(accessToken, request);
-            } else {
-                request.addHeader("Authorization", getAuthHeader());
+                isNewBpmProcess = true;
             }
+        }
+
+        HttpPost request = new HttpPost(uri);
+        String jsonEntity = buildResultRest != null ? buildResultRest.toFullLogString()
+                : "{\"error\", \"" + errMessage + "\"}";
+
+        if (isNewBpmProcess) {
+            StringEntity entity = new StringEntity(jsonEntity, StandardCharsets.UTF_8);
+            if (entity != null) {
+                request.setEntity(entity);
+                log.debug("Json response entity set to the post request for uri: " + uri);
+            } else {
+                log.error("Missing json response entity to post to: " + uri);
+            }
+            configureRequestNewBPM(accessToken, request);
         } else {
+            List<NameValuePair> parameters = new ArrayList<>();
+            parameters.add(new BasicNameValuePair("event", jsonEntity));
+
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(parameters, StandardCharsets.UTF_8);
+            if (entity != null) {
+                request.setEntity(entity);
+                log.debug("Urlencoded response entity set to the post request for uri: " + uri);
+            } else {
+                log.error("Missing urlencoded response entity to post to: " + uri);
+            }
             request.addHeader("Authorization", getAuthHeader());
         }
 
