fe5a0f1458a11251b7334f561d866be34d5f4f1b
Honza Brázdil <jbrazdil@redhat.com>
2021-06-22 14:46:11 +0200

    [NCL-6195] Add endpoint for Deliverable analysis results

---
+++ rest/src/main/java/org/jboss/pnc/rest/endpoints/internal/api/DeliverableAnalysisEndpoint.java
@@ -0,0 +1,57 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2020 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.rest.endpoints.internal.api;
+
+import static org.jboss.pnc.rest.configuration.SwaggerConstants.SUCCESS_CODE;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.pnc.api.deliverablesanalyzer.dto.AnalysisResult;
+import org.jboss.pnc.bpm.model.causeway.MilestoneReleaseResultRest;
+import org.jboss.pnc.dto.tasks.RepositoryCreationResult;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+/**
+ * This endpoint is used for interacting with Deliverable Analyzer processes.
+ *
+ * @author Honza Brázdil
+ */
+@Tag(name = "Internal")
+@Path("/deliverable-analysis")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface DeliverableAnalysisEndpoint {
+
+    @Operation(
+            summary = "Notify PNC about finished Deliverable anaylysis.",
+            responses = { @ApiResponse(responseCode = SUCCESS_CODE, description = "Success") })
+    @POST
+    @Path("/complete")
+    @Consumes(MediaType.APPLICATION_JSON)
+    void completeAnalysis(@Parameter(description = "Analysis result") AnalysisResult result);
+
+}
