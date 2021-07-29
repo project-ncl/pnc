8900bfb6f06a60185bf2983ad0020a503566e8b0
Petr Kocandrle <pkocandr@redhat.com>
2021-07-29 12:20:54 +0200

    Add dummy MetricsConfig implementation

---
+++ indy-repository-manager/src/main/java/org/jboss/pnc/indyrepositorymanager/IndyMetricsConfig.java
@@ -0,0 +1,82 @@
+package org.jboss.pnc.indyrepositorymanager;
+
+import org.commonjava.o11yphant.metrics.conf.ConsoleConfig;
+import org.commonjava.o11yphant.metrics.conf.ELKConfig;
+import org.commonjava.o11yphant.metrics.conf.GraphiteConfig;
+import org.commonjava.o11yphant.metrics.conf.MetricsConfig;
+import org.commonjava.o11yphant.metrics.conf.PrometheusConfig;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class IndyMetricsConfig implements MetricsConfig {
+
+    private String nodePrefix;
+
+    private boolean enabled;
+
+    private String reporter;
+
+    private ConsoleConfig consoleConfig;
+
+    private GraphiteConfig graphiteConfig;
+
+    private PrometheusConfig prometheusConfig;
+
+    private ELKConfig elkConfig;
+
+    private int meterRatio;
+
+    public IndyMetricsConfig() {
+        nodePrefix = "";
+        enabled = false;
+        reporter = null;
+        consoleConfig = null;
+        graphiteConfig = null;
+        prometheusConfig = null;
+        elkConfig = null;
+        meterRatio = 0;
+    }
+
+    @Override
+    public String getNodePrefix() {
+        return nodePrefix;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return enabled;
+    }
+
+    @Override
+    public String getReporter() {
+        return reporter;
+    }
+
+    @Override
+    public ConsoleConfig getConsoleConfig() {
+        return consoleConfig;
+    }
+
+    @Override
+    public GraphiteConfig getGraphiteConfig() {
+        return graphiteConfig;
+    }
+
+    @Override
+    public PrometheusConfig getPrometheusConfig() {
+        return prometheusConfig;
+    }
+
+    @Override
+    public ELKConfig getELKConfig() {
+        return elkConfig;
+    }
+
+    @Override
+    public int getMeterRatio() {
+        return meterRatio;
+    }
+
+}
