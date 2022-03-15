494a521ed101f386c2dfad287292c85693a17e51
Dustin Kut Moy Cheung <dcheung@redhat.com>
2022-03-15 14:25:53 -0400

    [NCL-6695] Add schema update for operation table

--- model/src/main/resources/db-update/20030-from-2.2.0-to-2.3.0.sql
+++ model/src/main/resources/db-update/20030-from-2.2.0-to-2.3.0.sql
@@ -24,3 +24,40 @@ BEGIN transaction;
     UPDATE buildrecord SET alignmentpreference = 'PREFER_TEMPORARY' WHERE temporarybuild IS TRUE AND alignmentpreference IS NULL;
     UPDATE buildconfigsetrecord SET alignmentpreference = 'PREFER_TEMPORARY' WHERE temporarybuild IS TRUE AND alignmentpreference IS NULL;
 COMMIT;
+
+-- NCL-6695: Add Operation table
+BEGIN;
+
+    CREATE TABLE operation (
+        operation_type varchar(50) NOT NULL,
+        id bigint NOT NULL,
+        endtime timestamptz,
+        progressstatus varchar(255),
+        result varchar(255),
+        starttime timestamptz,
+        submittime timestamptz,
+        user_id integer NOT NULL,
+        productmilestone_id integer,
+        primary key (id)
+    );
+
+    CREATE INDEX idx_operation_user ON operation (user_id);
+    ALTER TABLE operation ADD CONSTRAINT fk_operation_user
+    FOREIGN KEY (user_id) REFERENCES usertable(id);
+
+    CREATE INDEX idx_operation_milestone ON operation (productmilestone_id);
+    ALTER TABLE operation ADD CONSTRAINT fk_operation_productmilestone
+    FOREIGN KEY (productmilestone_id) REFERENCES productmilestone(id);
+
+
+    CREATE TABLE operation_parameters (
+        operation_id bigint NOT NULL,
+        value varchar(8192) NOT NULL,
+        key varchar(50) NOT NULL,
+        primary key (operation_id, key)
+    );
+
+    ALTER TABLE operation_parameters ADD CONSTRAINT fk_operation_parameters_operation
+    FOREIGN KEY (operation_id) REFERENCES operation(id);
+
+COMMIT;
