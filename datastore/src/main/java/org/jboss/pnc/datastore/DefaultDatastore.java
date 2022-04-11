e0e1b19e2d76032f6d8c2c2708b0804d7aad239c
Honza Brázdil <jbrazdil@redhat.com>
2022-04-11 20:57:27 +0200

    [NCLSUP-641] Fix artifact storing memory issues
    The problem was that when building the artifact cache, there were
    hundreds if not thousands of duplicate artifacts with the same sha256
    and identifier. This is caused by the way generic proxy artifacts are
    stored.
    
    One problem is that the DB query to form the cache is searching only by
    sha256 when searching by (sha256, identifier) tuple would be better. But
    I don't know how to fix this.
    
    But the artifact cache was not even used when storing the generic proxy
    artifacts! So we can safely ignore the generic proxy artifact when
    creating the cache.

--- datastore/src/main/java/org/jboss/pnc/datastore/DefaultDatastore.java
+++ datastore/src/main/java/org/jboss/pnc/datastore/DefaultDatastore.java
@@ -207,7 +207,9 @@ public class DefaultDatastore implements Datastore {
 
         Set<Artifact.IdentifierSha256> artifactConstraints = new HashSet<>();
         for (Artifact artifact : artifacts) {
-            artifactConstraints.add(new Artifact.IdentifierSha256(artifact.getIdentifier(), artifact.getSha256()));
+            if (!isGenericProxy(artifact)) { // There are thousands of duplicate artifacts that we don't want to cache
+                artifactConstraints.add(new Artifact.IdentifierSha256(artifact.getIdentifier(), artifact.getSha256()));
+            }
         }
 
         fetchOrSaveRequiredTargetRepositories(artifacts, storedTargetRepositories);
@@ -230,7 +232,7 @@ public class DefaultDatastore implements Datastore {
                     storedTargetRepositories.get(artifact.getTargetRepository().getIdentifierPath()));
 
             Artifact artifactFromDb;
-            if (RepositoryType.GENERIC_PROXY.equals(artifact.getTargetRepository().getRepositoryType())) {
+            if (isGenericProxy(artifact)) {
                 artifactFromDb = saveHttpArtifact(artifact);
             } else {
                 artifactFromDb = getOrSaveRepositoryArtifact(artifact, artifactCache);
@@ -241,6 +243,10 @@ public class DefaultDatastore implements Datastore {
 
         logger.debug("Artifacts saved: {}.", artifacts);
         return savedArtifacts;
+    }
+
+    private boolean isGenericProxy(Artifact artifact) {
+        return RepositoryType.GENERIC_PROXY.equals(artifact.getTargetRepository().getRepositoryType());
     }
 
     private void fetchOrSaveRequiredTargetRepositories(
