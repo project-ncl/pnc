eb99efea1cc5a33daf84cbc06fe7db06f5793e40
jmichalo <jmichalo@redhat.com>
2023-01-10 14:14:38 +0100

    [NCL-7424] add support for MP Config and create a scheduler @module

---
+++ moduleconfig/src/main/java/org/jboss/pnc/common/json/moduleconfig/SchedulerConfig.java
@@ -0,0 +1,44 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.common.json.moduleconfig;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.jboss.pnc.common.json.AbstractModuleConfig;
+
+@Getter
+public class SchedulerConfig extends AbstractModuleConfig {
+
+    public static final String MODULE_NAME = "scheduler-config";
+
+    private final String schedulerBaseUrl;
+    private final String connectTimeout;
+    private final String readTimeout;
+    private final String followRedirects;
+
+    public SchedulerConfig(
+            @JsonProperty("schedulerBaseUrl") String schedulerBaseUrl,
+            @JsonProperty("connectTimeout") String connectTimeout,
+            @JsonProperty("readTimeout") String readTimeout,
+            @JsonProperty("followRedirects") String followRedirects) {
+        this.schedulerBaseUrl = schedulerBaseUrl;
+        this.connectTimeout = connectTimeout;
+        this.readTimeout = readTimeout;
+        this.followRedirects = followRedirects;
+    }
+}
