d99603b4bf7f9b6a9c882e1961dd7d2ed5ab6f8f
Matej Lazar <matejonnet@gmail.com>
2023-04-13 14:50:23 +0200

    Get free port from maven task (used to configre EAP).

---
+++ integration-test-rex/src/main/java/org/jboss/pnc/integrationrex/GetFreePort.java
@@ -0,0 +1,55 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.integrationrex;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.util.Objects;
+import java.util.Properties;
+
+public class GetFreePort {
+
+    public static final String KEYCLOAK_PORT = "keycloakPort";
+
+    public static void main(String[] args) {
+        int freeHostPort = getFreeHostPort();
+        Properties properties = new Properties();
+        properties.put(KEYCLOAK_PORT, Objects.toString(freeHostPort));
+        try (final OutputStream outputstream = new FileOutputStream("target/test.properties")) {
+            properties.store(outputstream, "File Updated");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int getFreeHostPort() {
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            if (serverSocket == null) {
+                throw new RuntimeException("Cannot open socket.");
+            }
+            if (serverSocket.getLocalPort() <= 0) {
+                throw new RuntimeException("Cannot get port.");
+            }
+            return serverSocket.getLocalPort();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
