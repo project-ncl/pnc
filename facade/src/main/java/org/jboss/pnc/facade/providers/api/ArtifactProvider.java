06967a30e0ba9b865865341bdc7ffa613bef62b9
jmichalo <jmichalo@redhat.com>
2023-04-25 15:31:50 +0200

    [Alignment 2.0] add Qualifier specialization into ArtifactInfo endpoint

--- facade/src/main/java/org/jboss/pnc/facade/providers/api/ArtifactProvider.java
+++ facade/src/main/java/org/jboss/pnc/facade/providers/api/ArtifactProvider.java
@@ -20,6 +20,7 @@ package org.jboss.pnc.facade.providers.api;
 import org.jboss.pnc.dto.Artifact;
 import org.jboss.pnc.dto.ArtifactRef;
 import org.jboss.pnc.dto.ArtifactRevision;
+import org.jboss.pnc.dto.requests.QValue;
 import org.jboss.pnc.dto.response.ArtifactInfo;
 import org.jboss.pnc.dto.response.Page;
 import org.jboss.pnc.enums.ArtifactQuality;
@@ -47,8 +48,8 @@ public interface ArtifactProvider
             Optional<String> identifierPattern,
             Set<ArtifactQuality> qualities, // default value is empty Set
             Optional<RepositoryType> repoType,
-            Set<BuildCategory> buildCategories // default value is empty Set
-    );
+            Set<BuildCategory> buildCategories, // default value is empty Set
+            Set<QValue> requestedQualifiers);
 
     Page<Artifact> getBuiltArtifactsForBuild(
             int pageIndex,
