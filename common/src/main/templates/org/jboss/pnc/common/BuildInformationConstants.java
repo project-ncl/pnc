596cb8d4398fd6c4620b21c49239c1cb470590b7
Dustin Kut Moy Cheung <dcheung@redhat.com>
2023-06-19 16:18:47 -0400

    [NCL-6709] Introduce /rest/v2/version endpoint

---
+++ common/src/main/templates/org/jboss/pnc/common/BuildInformationConstants.java
@@ -0,0 +1,31 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.environmentdriver;
+
+/**
+ * The values in here are auto-generated from Maven plugin maven-replacer-plugin and the template is found in the
+ * template folder
+ *
+ * @author Dustin Kut Moy Cheung <dcheung@redhat.com>
+ */
+public final class BuildInformationConstants {
+
+    public static final String VERSION = "@version@";
+    public static final String COMMIT_HASH = "@commit-id@";
+    public static final String BUILD_TIME = "@build-time@";
+}
