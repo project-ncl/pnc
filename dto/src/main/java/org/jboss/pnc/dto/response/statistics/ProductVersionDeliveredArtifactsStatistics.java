2cc7d37f98bc52c4ef84a8cda55233692eba35dc
Adam Krídl <akridl@redhat.com>
2023-07-31 15:47:20 +0200

    [NCL-7651]: Introduce ProductVersionDeliveredArtifactsStatistics

---
+++ dto/src/main/java/org/jboss/pnc/dto/response/statistics/ProductVersionDeliveredArtifactsStatistics.java
@@ -0,0 +1,63 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.dto.response.statistics;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+/**
+ * Statistics about the delivered artifacts of a version.
+ *
+ * @author Adam Kridl &lt;akridl@redhat.com&gt;
+ */
+@Value
+@Builder(builderClassName = "Builder")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Jacksonized
+public class ProductVersionDeliveredArtifactsStatistics {
+
+    /**
+     * Number of Delivered Artifacts of Milestones of this Version produced by Builds contained in Milestones of this
+     * Version.
+     */
+    Long thisVersion;
+
+    /**
+     * Number of Delivered Artifacts of Milestones of this Version produced by Builds contained in Milestones of
+     * previous Versions of the same Product.
+     */
+    Long otherVersions;
+
+    /**
+     * Number of Delivered Artifacts of Milestones of this Version produced by Builds contained in Milestones of other
+     * Products.
+     */
+    Long otherProducts;
+
+    /**
+     * Number of Delivered Artifacts of Milestones of this Version produced by Builds not contained in any Milestone.
+     */
+    Long noMilestone;
+
+    /**
+     * Number of Delivered Artifacts of Milestones of this Version not produced in any Build.
+     */
+    Long noBuild;
+}
