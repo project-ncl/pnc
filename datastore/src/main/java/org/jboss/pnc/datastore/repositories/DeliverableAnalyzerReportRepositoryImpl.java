3101f5e4440bfcdb85bc300aef24ad98b5c0e33b
Adam Krídl <akridl@redhat.com>
2023-09-05 16:57:25 +0200

    [NCL-8071]: Add methods to DeliverableAnalyzerReportRepository in order to handle /deliver-analysis/{id}/analyzed-artifacts

--- datastore/src/main/java/org/jboss/pnc/datastore/repositories/DeliverableAnalyzerReportRepositoryImpl.java
+++ datastore/src/main/java/org/jboss/pnc/datastore/repositories/DeliverableAnalyzerReportRepositoryImpl.java
@@ -19,10 +19,18 @@ package org.jboss.pnc.datastore.repositories;
 
 import org.jboss.pnc.datastore.repositories.internal.AbstractRepository;
 import org.jboss.pnc.model.Base32LongID;
+import org.jboss.pnc.model.DeliverableAnalyzerOperation_;
 import org.jboss.pnc.model.DeliverableAnalyzerReport;
+import org.jboss.pnc.model.DeliverableAnalyzerReport_;
+import org.jboss.pnc.model.DeliverableArtifact;
+import org.jboss.pnc.model.DeliverableArtifact_;
 import org.jboss.pnc.spi.datastore.repositories.DeliverableAnalyzerReportRepository;
 
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Stateless
 public class DeliverableAnalyzerReportRepositoryImpl extends AbstractRepository<DeliverableAnalyzerReport, Base32LongID>
@@ -30,5 +38,41 @@ public class DeliverableAnalyzerReportRepositoryImpl extends AbstractRepository<
 
     public DeliverableAnalyzerReportRepositoryImpl() {
         super(DeliverableAnalyzerReport.class, Base32LongID.class);
+    }
+
+    @Override
+    public List<DeliverableArtifact> getAnalyzedArtifacts(Base32LongID id) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<DeliverableArtifact> query = cb.createQuery(DeliverableArtifact.class);
+
+        Root<DeliverableArtifact> analyzedArtifacts = query.from(DeliverableArtifact.class);
+
+        query.select(analyzedArtifacts);
+        query.where(
+                cb.equal(
+                        analyzedArtifacts.get(DeliverableArtifact_.report)
+                                .get(DeliverableAnalyzerReport_.operation)
+                                .get(DeliverableAnalyzerOperation_.id),
+                        id));
+
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public int countAnalyzedArtifacts(Base32LongID id) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> query = cb.createQuery(Long.class);
+
+        Root<DeliverableArtifact> analyzedArtifacts = query.from(DeliverableArtifact.class);
+
+        query.select(cb.count(analyzedArtifacts));
+        query.where(
+                cb.equal(
+                        analyzedArtifacts.get(DeliverableArtifact_.report)
+                                .get(DeliverableAnalyzerReport_.operation)
+                                .get(DeliverableAnalyzerOperation_.id),
+                        id));
+
+        return entityManager.createQuery(query).getSingleResult().intValue();
     }
 }
