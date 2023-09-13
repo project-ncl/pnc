ed713a5e2f72e2f273b89f32c70eb547e2c7dbca
Adam Krídl <akridl@redhat.com>
2023-09-13 16:30:12 +0200

    [NCL-8070]: Introduce DeliverableAnalyzerReportLabelModifier
    This class knows the rules of adding (removing) new (old) label to (from) labels

---
+++ facade/src/main/java/org/jboss/pnc/facade/validation/labels/DeliverableAnalyzerReportLabelModifier.java
@@ -0,0 +1,69 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.facade.validation.labels;
+
+import org.jboss.pnc.api.enums.DeliverableAnalyzerReportLabel;
+import org.jboss.pnc.api.enums.LabelOperation;
+
+import javax.ejb.Stateless;
+import java.util.EnumSet;
+
+/**
+ * {@link LabelModifier} for {@link DeliverableAnalyzerReportLabel} entity.
+ */
+@Stateless
+public class DeliverableAnalyzerReportLabelModifier implements LabelModifier<DeliverableAnalyzerReportLabel> {
+
+    @Override
+    public void addLabel(DeliverableAnalyzerReportLabel label, EnumSet<DeliverableAnalyzerReportLabel> labels) throws InvalidLabelOperationException {
+        checkLabelIsNotPresent(label, labels);
+
+        switch (label) {
+            case DELETED:
+                labels.add(label);
+                break;
+            case RELEASED:
+                if (labels.contains(DeliverableAnalyzerReportLabel.DELETED)) {
+                    throw new InvalidLabelOperationException(label, labels, LabelOperation.ADDED, "cannot mark as RELEASED the report which is already marked DELETED");
+                } else if (labels.contains(DeliverableAnalyzerReportLabel.SCRATCH)) {
+                    throw new InvalidLabelOperationException(label, labels, LabelOperation.ADDED, "cannot mark as RELEASED the report which is already marked SCRATCH");
+                }
+                labels.add(label);
+                break;
+            case SCRATCH:
+                throw new InvalidLabelOperationException(label, labels, LabelOperation.ADDED, "label can be marked as 'SCRATCH' only when the analysis is executed");
+            default:
+                throw new UnsupportedOperationException("Adding of label " + label + " is not supported");
+        }
+    }
+
+    @Override
+    public void removeLabel(DeliverableAnalyzerReportLabel label, EnumSet<DeliverableAnalyzerReportLabel> labels) throws InvalidLabelOperationException {
+        checkLabelIsPresent(label, labels);
+
+        switch (label) {
+            case DELETED:
+            case RELEASED:
+            case SCRATCH:
+                labels.remove(label);
+                break;
+            default:
+                throw new UnsupportedOperationException("Deleting of label " + label + " is not supported");
+        }
+    }
+}
---
+++ facade/src/main/java/org/jboss/pnc/facade/validation/labels/LabelModifier.java
@@ -0,0 +1,47 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.facade.validation.labels;
+
+import org.jboss.pnc.api.enums.LabelOperation;
+
+import java.util.EnumSet;
+
+/**
+ * The class implementing this interface is able to add (remove) new (old) label to (from) the set of active labels.
+ * Such a class complies with the rules of adding (removing) label for the given entity type E.
+ *
+ * @param <E> entity
+ */
+public interface LabelModifier<E extends Enum<E>> {
+
+    void addLabel(E label, EnumSet<E> labels) throws InvalidLabelOperationException;
+
+    void removeLabel(E label, EnumSet<E> labels) throws InvalidLabelOperationException;
+
+    default void checkLabelIsNotPresent(E label, EnumSet<E> labels) {
+        if (labels.contains(label)) {
+            throw new InvalidLabelOperationException(label, labels, LabelOperation.ADDED, "label already present in the set of active labels");
+        }
+    }
+
+    default void checkLabelIsPresent(E label, EnumSet<E> labels) {
+        if (!labels.contains(label)) {
+            throw new InvalidLabelOperationException(label, labels, LabelOperation.REMOVED, "no such label present in the set of active labels");
+        }
+    }
+}
