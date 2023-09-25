8d9ea23aeffa18a256a123f5dbf32f3c1390f938
jmichalo <jmichalo@redhat.com>
2023-09-25 17:42:17 +0200

    [NCL-8156] stable sort by submitTime for Build endpoints

--- spi/src/main/java/org/jboss/pnc/spi/datastore/repositories/api/SortInfo.java
+++ spi/src/main/java/org/jboss/pnc/spi/datastore/repositories/api/SortInfo.java
@@ -17,8 +17,11 @@
  */
 package org.jboss.pnc.spi.datastore.repositories.api;
 
+import javax.persistence.metamodel.SingularAttribute;
 import java.util.List;
 
 public interface SortInfo<T> {
     List<OrderInfo<T>> orders();
+
+    SortInfo<T> thenOrderBy(SingularAttribute<T, ? > attribute, OrderInfo.SortingDirection direction);
 }
