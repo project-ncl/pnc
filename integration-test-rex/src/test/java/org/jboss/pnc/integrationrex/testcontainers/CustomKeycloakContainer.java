19e20fa3306983aef969dc0286af3e5ab165f7b3
jmichalo <jmichalo@redhat.com>
2023-10-02 21:24:19 +0200

    fix Rex IT tests and do a small refactor

--- integration-test-rex/src/test/java/org/jboss/pnc/integrationrex/testcontainers/CustomKeycloakContainer.java
+++ integration-test-rex/src/test/java/org/jboss/pnc/integrationrex/testcontainers/CustomKeycloakContainer.java
@@ -17,79 +17,53 @@
  */
 package org.jboss.pnc.integrationrex.testcontainers;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
-import org.keycloak.admin.client.Keycloak;
+import lombok.SneakyThrows;
+import org.jboss.pnc.common.json.JsonOutputConverterMapper;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.UncheckedIOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomKeycloakContainer extends KeycloakContainer {
 
-    private final Set<String> importFiles = new HashSet<>();
-    private ObjectMapper objectMapper;
-
     public CustomKeycloakContainer() {
         super("quay.io/keycloak/keycloak:21.1.0");
-        initObjectMapper();
     }
 
     public CustomKeycloakContainer(String imageName) {
         super(imageName);
-        initObjectMapper();
     }
 
-    /**
-     * Customize objectMapper to accept exported realm file
-     */
-    private void initObjectMapper() {
-        objectMapper = new ObjectMapper();
-        objectMapper.configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
-
-    @Override
-    public KeycloakContainer withRealmImportFile(String importFile) {
-        // Parent importFiles are not accessible.
-        this.importFiles.add(importFile);
-        return self();
-    }
-
-    /**
-     * Use customized objectMapper.
-     */
+    @SneakyThrows
     @Override
     protected void containerIsStarted(InspectContainerResponse containerInfo, boolean reused) {
         if (reused) {
             logger().info("This container is being reused, so we're skipping the realm import.");
             return;
         }
-        if (!importFiles.isEmpty()) {
-            logger().info("Connect to Keycloak container to import given realm files.");
-            Keycloak kcAdmin = getKeycloakAdminClient();
-            try {
-                for (String importFile : importFiles) {
-                    logger().info("Importing realm from file {}", importFile);
-                    InputStream resourceStream = this.getClass().getResourceAsStream(importFile);
-                    // this is a dirty hack, but in certain cases, we need to obtain the resource stream from the
-                    // current thread context classloader
-                    // as soon as the auto-import of realm files returns again (approx. KC 18), this complete method is
-                    // removed anyway.
-                    if (resourceStream == null) {
-                        resourceStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(importFile);
-                    }
-                    kcAdmin.realms().create(objectMapper.readValue(resourceStream, RealmRepresentation.class));
-                }
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
+
+        InputStream realmStream = Thread.currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream("keycloak-realm-export.json");
+
+        RealmRepresentation realmRepresentation = JsonOutputConverterMapper
+                .readValue(realmStream, RealmRepresentation.class);
+
+        List<RoleRepresentation> defaultRoles = realmRepresentation.getRoles()
+                .getRealm()
+                .stream()
+                .filter(rl -> rl.getName().startsWith("pnc-"))
+                .collect(Collectors.toList());
+
+        getKeycloakAdminClient().realm("newcastle-testcontainer")
+                .roles()
+                .get("default-roles-newcastle-testcontainer")
+                .addComposites(defaultRoles);
+
     }
+
 }
