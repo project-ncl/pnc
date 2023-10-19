6530283550b6a83e6cf1951a3c5c26f60d2bd4dc
Honza Brázdil <jbrazdil@redhat.com>
2023-10-19 16:09:03 +0200

    Split DB migration script for 2.6/2.7

--- model/src/main/resources/db-update/20060-from-2.5.0-to-2.6.0.sql
+++ model/src/main/resources/db-update/20060-from-2.5.0-to-2.6.0.sql
@@ -17,52 +17,6 @@
 --
 
 BEGIN;
-    ALTER TABLE buildconfigsetrecord ALTER COLUMN id SET DATA TYPE bigint;
-    -- update foreign bcsr keys
-    ALTER TABLE buildrecord ALTER COLUMN buildconfigsetrecord_id TYPE bigint;
-    ALTER TABLE build_config_set_record_attributes ALTER COLUMN build_config_set_record_id TYPE bigint;
-    -- update archived build records foreign key
-    ALTER TABLE _archived_buildrecords ALTER COLUMN buildconfigsetrecord_id TYPE bigint;
-
-    CREATE TABLE build_configuration_align_strats
-    (
-        buildconfiguration_id integer                NOT NULL,
-        allowlist             text,
-        denylist              text,
-        idallowlist           text,
-        iddenylist            text,
-        idranks               text,
-        ranks                 text,
-        dependencyscope       character varying(255) NOT NULL
-    );
-
-    ALTER TABLE build_configuration_align_strats
-        ADD CONSTRAINT build_configuration_align_strats_pkey PRIMARY KEY (buildconfiguration_id, dependencyscope);
-
-    ALTER TABLE build_configuration_align_strats
-        ADD CONSTRAINT fk_align_strats_bc FOREIGN KEY (buildconfiguration_id) REFERENCES buildconfiguration(id);
-
-    CREATE TABLE build_configuration_align_strats_aud
-    (
-        rev                   integer                NOT NULL,
-        revtype               smallint               NOT NULL,
-        buildconfiguration_id integer                NOT NULL,
-        dependencyscope       character varying(255) NOT NULL,
-        ranks                 text,
-        idallowlist           text,
-        denylist              text,
-        iddenylist            text,
-        idranks               text,
-        allowlist             text
-    );
-    ALTER TABLE build_configuration_align_strats_aud
-        ADD CONSTRAINT build_configuration_align_strats_aud_pkey PRIMARY KEY (rev, revtype, buildconfiguration_id, dependencyscope);
-
-    ALTER TABLE build_configuration_align_strats_aud
-        ADD CONSTRAINT fk_build_configuration_align_strats_aud_revinfo FOREIGN KEY (rev) REFERENCES revinfo(rev);
-
-    ALTER TABLE build_configuration_align_strats_aud
-        ADD CONSTRAINT fk_buildconfiguration_align_strats_aud_buildconfiguration FOREIGN KEY (buildconfiguration_id, rev) REFERENCES buildconfiguration_aud(id, rev);
 
     ALTER TABLE buildrecord ADD COLUMN scmbuildconfigrevision varchar(255);
     ALTER TABLE buildrecord ADD COLUMN scmbuildconfigrevisioninternal boolean;
