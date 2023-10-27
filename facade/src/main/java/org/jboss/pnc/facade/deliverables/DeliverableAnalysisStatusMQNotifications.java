df902dff963226af13db52be404d1f5882fdcf98
Andrea Vibelli <avibelli@redhat.com>
2023-10-27 17:02:52 +0200

    [NCL-8260] - Improve the JMS content sent after analysis status change

--- facade/src/main/java/org/jboss/pnc/facade/deliverables/DeliverableAnalysisStatusMQNotifications.java
+++ facade/src/main/java/org/jboss/pnc/facade/deliverables/DeliverableAnalysisStatusMQNotifications.java
@@ -56,6 +56,7 @@ public class DeliverableAnalysisStatusMQNotifications {
 
     private void send(MessageSender ms, DeliverableAnalysisStatusChangedEvent event) {
         Message message = new AnalysisStatusMessage(
+                event.getOperationId(),
                 ATTRIBUTE_NAME,
                 event.getMilestoneId(),
                 event.getStatus().toString(),
@@ -69,6 +70,7 @@ public class DeliverableAnalysisStatusMQNotifications {
         headers.put("attribute", ATTRIBUTE_NAME);
         headers.put("milestoneId", event.getMilestoneId());
         headers.put("status", event.getStatus().toString());
+        headers.put("operationId", event.getOperationId());
         return headers;
     }
 }
