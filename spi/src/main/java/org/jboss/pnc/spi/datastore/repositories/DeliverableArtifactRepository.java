0a4c0f7ffb9e0b8ac6b8bb17b142dd7852c0183d
Adam Krídl <akridl@redhat.com>
2023-12-18 19:39:48 +0100

    [NCL-8329]: Computation of Product Milestones statistics using new API

--- spi/src/main/java/org/jboss/pnc/spi/datastore/repositories/DeliverableArtifactRepository.java
+++ spi/src/main/java/org/jboss/pnc/spi/datastore/repositories/DeliverableArtifactRepository.java
@@ -17,13 +17,53 @@
  */
 package org.jboss.pnc.spi.datastore.repositories;
 
+import org.jboss.pnc.enums.ArtifactQuality;
+import org.jboss.pnc.enums.RepositoryType;
 import org.jboss.pnc.model.DeliverableArtifact;
 import org.jboss.pnc.model.DeliverableArtifactPK;
 import org.jboss.pnc.spi.datastore.repositories.api.Repository;
+
+import java.util.EnumMap;
 
 /**
  * Interface for manipulating with {@link DeliverableArtifact} entity
  */
 public interface DeliverableArtifactRepository extends Repository<DeliverableArtifact, DeliverableArtifactPK> {
 
+    /**
+     * Number of artifacts delivered in product milestone given by id, **which were also built in that milestone**.
+     */
+    long countDeliveredArtifactsBuiltInThisMilestone(Integer productMilestoneId);
+
+    /**
+     * Number of artifacts delivered in product milestone given by id, **which were built in another milestone (but same
+     * product)**.
+     */
+    long countDeliveredArtifactsBuiltInOtherMilestones(Integer productMilestoneId);
+
+    /**
+     * Number of artifacts delivered in product milestone given by id, **which were built in any milestone of different
+     * product**.
+     */
+    long countDeliveredArtifactsBuiltByOtherProducts(Integer productMilestoneId);
+
+    /**
+     * Number of artifacts delivered in product milestone given by id, **whose build does not belong to any milestone**.
+     */
+    long countDeliveredArtifactsBuiltInNoMilestone(Integer productMilestoneId);
+
+    /**
+     * Number of artifacts delivered in product milestone given by id, **which were not built, i.e. were imported**.
+     */
+    long countDeliveredArtifactsNotBuilt(Integer productMilestoneId);
+
+    /**
+     * Artifact qualities of **all delivered artifacts in the milestone given by id**.
+     */
+    EnumMap<ArtifactQuality, Long> getArtifactQualitiesCounts(Integer productMilestoneId);
+
+    /**
+     * Repository types of **all delivered artifacts in the milestone given by id**.
+     */
+    EnumMap<RepositoryType, Long> getRepositoryTypesCounts(Integer productMilestoneId);
 }
