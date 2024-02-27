e243b8fa6ff1578c2b651f4e31e9082f1a283dc0
Adam Krídl <akridl@redhat.com>
2024-02-27 16:45:17 +0100

    [NCL-8434]: Implement sending of the whole DelAn operation in the notification

--- notifications/src/main/java/org/jboss/pnc/notification/DefaultEventObserver.java
+++ notifications/src/main/java/org/jboss/pnc/notification/DefaultEventObserver.java
@@ -17,25 +17,28 @@
  */
 package org.jboss.pnc.notification;
 
-import java.lang.invoke.MethodHandles;
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
-
 import org.jboss.pnc.dto.BuildPushResult;
+import org.jboss.pnc.dto.Operation;
 import org.jboss.pnc.dto.ProductMilestoneCloseResult;
 import org.jboss.pnc.dto.notification.BuildChangedNotification;
 import org.jboss.pnc.dto.notification.BuildPushResultNotification;
 import org.jboss.pnc.dto.notification.GroupBuildChangedNotification;
 import org.jboss.pnc.dto.notification.OperationNotification;
 import org.jboss.pnc.dto.notification.ProductMilestoneCloseResultNotification;
+import org.jboss.pnc.mapper.api.DeliverableAnalyzerOperationMapper;
 import org.jboss.pnc.model.DeliverableAnalyzerOperation;
 import org.jboss.pnc.notification.dist.DistributedEventHandler;
+import org.jboss.pnc.spi.datastore.repositories.DeliverableAnalyzerOperationRepository;
 import org.jboss.pnc.spi.events.BuildSetStatusChangedEvent;
 import org.jboss.pnc.spi.events.BuildStatusChangedEvent;
 import org.jboss.pnc.spi.events.OperationChangedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.lang.invoke.MethodHandles;
 
 /**
  * Observe build events
@@ -47,6 +50,12 @@ public class DefaultEventObserver {
 
     @Inject
     DistributedEventHandler handler;
+
+    @Inject
+    DeliverableAnalyzerOperationRepository deliverableAnalyzerOperationRepository;
+
+    @Inject
+    DeliverableAnalyzerOperationMapper deliverableAnalyzerOperationMapper;
 
     private void sendMessage(Object msg) {
         handler.sendEvent(msg);
@@ -85,18 +94,27 @@ public class DefaultEventObserver {
     public void collectOperationChangedEvent(@Observes OperationChangedEvent operationChangedEvent) {
         logger.trace("Observed new OperationChangedEvent event {}.", operationChangedEvent);
         String notificationType;
+        Operation operationToSend;
         if (operationChangedEvent.getOperationClass() == DeliverableAnalyzerOperation.class) {
             notificationType = "DELIVERABLES_ANALYSIS";
+            DeliverableAnalyzerOperation deliverableAnalyzerOperation = deliverableAnalyzerOperationRepository
+                    .queryById(operationChangedEvent.getId());
+            deliverableAnalyzerOperation.setProgressStatus(operationChangedEvent.getStatus());
+            deliverableAnalyzerOperation.setResult(operationChangedEvent.getResult());
+            operationToSend = deliverableAnalyzerOperationMapper.toDTO(deliverableAnalyzerOperation);
         } else {
             notificationType = "UNKNOWN-OPERATION";
+            operationToSend = null;
         }
+
         sendMessage(
                 new OperationNotification(
                         notificationType,
                         operationChangedEvent.getId().toString(),
                         operationChangedEvent.getStatus(),
                         operationChangedEvent.getPreviousStatus(),
-                        operationChangedEvent.getResult()));
+                        operationChangedEvent.getResult(),
+                        operationToSend));
         logger.trace("OperationChangedEvent event processed {}.", operationChangedEvent);
     }
 
