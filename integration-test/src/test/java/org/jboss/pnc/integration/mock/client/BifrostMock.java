36814a7f5953b05e2bbf9257ba5ac59703406ab5
Honza Brázdil <jbrazdil@redhat.com>
2024-03-25 13:14:23 +0100

    Mock Bifrost in build Brew Push test

---
+++ integration-test/src/test/java/org/jboss/pnc/integration/mock/client/BifrostMock.java
@@ -0,0 +1,89 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.integration.mock.client;
+
+import org.jboss.pnc.api.bifrost.dto.Line;
+import org.jboss.pnc.api.bifrost.dto.MetaData;
+import org.jboss.pnc.api.bifrost.enums.Direction;
+import org.jboss.pnc.api.bifrost.enums.Format;
+import org.jboss.pnc.api.bifrost.rest.Bifrost;
+import org.jboss.pnc.api.causeway.dto.push.BuildImportRequest;
+import org.jboss.pnc.api.causeway.dto.untag.UntagRequest;
+import org.jboss.pnc.api.dto.ComponentVersion;
+import org.jboss.pnc.causewayclient.CausewayClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.List;
+
+@ApplicationScoped
+public class BifrostMock implements Bifrost {
+
+    @Override
+    public Response getAllLines(
+            String matchFilters,
+            String prefixFilters,
+            Line afterLine,
+            Direction direction,
+            Format format,
+            Integer maxLines,
+            Integer batchSize,
+            Integer batchDelay,
+            boolean follow,
+            String timeoutProbeString) {
+        return null;
+    }
+
+    @Override
+    public List<Line> getLines(
+            String matchFilters,
+            String prefixFilters,
+            Line afterLine,
+            Direction direction,
+            Integer maxLines,
+            Integer batchSize) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Response getFinalLog(String processContext, String tag) {
+        return null;
+    }
+
+    @Override
+    public long getFinalLogSize(String buildId, String tag) {
+        return 0;
+    }
+
+    @Override
+    public MetaData getMetaData(
+            String matchFilters,
+            String prefixFilters,
+            Line afterLine,
+            Direction direction,
+            Integer maxLines,
+            Integer batchSize) throws IOException {
+        return null;
+    }
+
+    @Override
+    public ComponentVersion getVersion() {
+        return null;
+    }
+}
