e60446b5a84169552ea248ca5d4920b507fea9eb
jmichalo <jmichalo@redhat.com>
2024-03-27 13:51:44 +0100

    [NCL-8539] refactor gerrit provider and add support for gitlab archive

--- bpm/src/main/java/org/jboss/pnc/bpm/causeway/BuildResultPushManager.java
+++ bpm/src/main/java/org/jboss/pnc/bpm/causeway/BuildResultPushManager.java
@@ -33,8 +33,8 @@ import org.jboss.pnc.api.dto.Request;
 import org.jboss.pnc.bpm.InvalidReferenceException;
 import org.jboss.pnc.bpm.MissingInternalReferenceException;
 import org.jboss.pnc.causewayclient.CausewayClient;
-import org.jboss.pnc.common.gerrit.Gerrit;
-import org.jboss.pnc.common.gerrit.GerritException;
+import org.jboss.pnc.common.gerrit.ScmUrlGeneratorProvider;
+import org.jboss.pnc.common.gerrit.ScmException;
 import org.jboss.pnc.common.logging.MDCUtils;
 import org.jboss.pnc.common.maven.Gav;
 import org.jboss.pnc.dto.BuildPushResult;
@@ -70,6 +70,7 @@ import java.util.stream.Collectors;
 
 import static org.jboss.pnc.api.constants.Attributes.BUILD_BREW_NAME;
 import static org.jboss.pnc.api.constants.BuildConfigurationParameterKeys.BREW_BUILD_NAME;
+import static org.jboss.pnc.common.gerrit.ScmUrlGeneratorProvider.SCMProvider.GERRIT;
 
 /**
  * @author <a href="mailto:matejonnet@gmail.com">Matej Lazar</a>
@@ -92,7 +93,7 @@ public class BuildResultPushManager {
 
     private Event<BuildPushResult> buildPushResultEvent;
 
-    private Gerrit gerrit;
+    private ScmUrlGeneratorProvider scmUrlGenerator;
 
     private Logger logger = LoggerFactory.getLogger(BuildResultPushManager.class);
 
@@ -108,7 +109,7 @@ public class BuildResultPushManager {
             InProgress inProgress,
             Event<BuildPushResult> buildPushResultEvent,
             ArtifactRepository artifactRepository,
-            Gerrit gerrit,
+            ScmUrlGeneratorProvider scmUrlGenerator,
             CausewayClient causewayClient) {
         this.buildConfigurationAuditedRepository = buildConfigurationAuditedRepository;
         this.buildRecordPushResultRepository = buildRecordPushResultRepository;
@@ -116,7 +117,7 @@ public class BuildResultPushManager {
         this.inProgress = inProgress;
         this.buildPushResultEvent = buildPushResultEvent;
         this.artifactRepository = artifactRepository;
-        this.gerrit = gerrit;
+        this.scmUrlGenerator = scmUrlGenerator;
         this.causewayClient = causewayClient;
     }
 
@@ -258,9 +259,12 @@ public class BuildResultPushManager {
 
     private String getSourcesUrl(BuildRecord buildRecord) {
         try {
-            return gerrit
-                    .generateDownloadUrlWithGerritGitweb(buildRecord.getScmRepoURL(), buildRecord.getScmRevision());
-        } catch (GerritException e) {
+            var provider = scmUrlGenerator.determineScmProvider(
+                    buildRecord.getScmRepoURL(),
+                    buildRecord.getBuildConfigurationAudited().getRepositoryConfiguration().getInternalUrl());
+            return scmUrlGenerator.getScmUrlGenerator(provider)
+                    .generateDownloadUrlWithGitweb(buildRecord.getScmRepoURL(), buildRecord.getScmRevision());
+        } catch (ScmException e) {
             throw new RuntimeException("Failed to get SCM url from gerrit", e);
         }
     }
