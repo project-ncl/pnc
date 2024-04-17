fa34e5946b8f834f4390a2835a3e1e93471af684
Jan Michalov <jmichalo@redhat.com>
2024-04-17 12:48:16 +0200

    Revert "[Alignment 2.0] implement artifact info qualifier tests"
    This reverts commit 3b0741e5

--- facade/src/main/java/org/jboss/pnc/facade/providers/ArtifactProviderImpl.java
+++ facade/src/main/java/org/jboss/pnc/facade/providers/ArtifactProviderImpl.java
@@ -204,6 +204,7 @@ public class ArtifactProviderImpl
         List<Tuple> list = doNativeQuery(outerQuery, queryParams);
 
         List<ArtifactInfo> artifacts = list.stream()
+                .peek((object) -> log.debug(object.toString()))
                 .collect(groupingBy(tuple -> tuple.get("identifier", String.class)))
                 .values()
                 .stream()
