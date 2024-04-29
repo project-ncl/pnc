d3f273e4edb6bdfdae2bddb6145b71d612fd1e1f
Honza Brázdil <jbrazdil@redhat.com>
2024-04-29 20:36:30 +0200

    Update mockito version
    Mockito 5.2.0+ has issues with our inject mocks

--- facade/src/test/java/org/jboss/pnc/facade/rsql/RSQLPredicateProducerTest.java
+++ facade/src/test/java/org/jboss/pnc/facade/rsql/RSQLPredicateProducerTest.java
@@ -38,7 +38,7 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 import javax.persistence.criteria.CriteriaBuilder;
