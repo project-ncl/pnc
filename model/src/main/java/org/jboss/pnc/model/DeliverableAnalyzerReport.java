c9a1b36da599f768254d7295084bc93d4aa8a84e
Honza Brázdil <jbrazdil@redhat.com>
2025-01-22 16:15:37 +0100

    Fix toString infinite recursion in Deliverable Analyzer

--- model/src/main/java/org/jboss/pnc/model/DeliverableAnalyzerReport.java
+++ model/src/main/java/org/jboss/pnc/model/DeliverableAnalyzerReport.java
@@ -110,7 +110,6 @@ public class DeliverableAnalyzerReport implements GenericEntity<Base32LongID> {
 
     @Override
     public String toString() {
-        return "DeliverableAnalyzerReport{" + "id=" + id + ", operation=" + operation + ", labels=" + labels
-                + ", labelHistory=" + labelHistory + ", artifacts=" + artifacts + '}';
+        return "DeliverableAnalyzerReport{" + "id=" + id + ", operation=" + operation + ", labels=" + labels + '}';
     }
 }
