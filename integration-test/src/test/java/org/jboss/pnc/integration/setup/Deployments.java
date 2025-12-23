e5d47ebdd8d7a2d5f070bd1258c3300be2730c58
jmichalo <jmichalo@redhat.com>
2025-12-23 15:00:50 +0100

    [NCL-9404] remove old BPM code

--- integration-test/src/test/java/org/jboss/pnc/integration/setup/Deployments.java
+++ integration-test/src/test/java/org/jboss/pnc/integration/setup/Deployments.java
@@ -57,7 +57,6 @@ public class Deployments {
     public static final String CAUSEWAY_CLIENT_JAR = "/causeway-client.jar";
     public static final String DINGROGU_CLIENT_JAR = "/lib/dingrogu-client.jar";
 
-    public static final String BPM_JAR = "/bpm.jar";
     public static final String REST_WAR = "/rest.war";
 
     private static final PomEquippedResolveStage resolver = Maven.resolver()
@@ -193,7 +192,7 @@ public class Deployments {
         jar.addClass(RepositoryManagerResultMock.class);
         jar.addClass(ArtifactBuilder.class);
 
-        jar.addAsManifestResource("beans-use-mock-remote-clients.xml", "beans.xml");
+        jar.addAsManifestResource("beans-use-mock-remote-clients-and-local-scheduler.xml", "beans.xml");
 
         logger.info(jar.toString(true));
 
