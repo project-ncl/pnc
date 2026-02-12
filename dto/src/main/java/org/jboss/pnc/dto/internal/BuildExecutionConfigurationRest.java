5325897cf913f16cafab4a6e7c1b471a2bb0e515
jmichalo <jmichalo@redhat.com>
2026-02-12 13:21:14 +0100

    headers

--- dto/src/main/java/org/jboss/pnc/dto/internal/BuildExecutionConfigurationRest.java
+++ dto/src/main/java/org/jboss/pnc/dto/internal/BuildExecutionConfigurationRest.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jboss.pnc.dto.internal;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
