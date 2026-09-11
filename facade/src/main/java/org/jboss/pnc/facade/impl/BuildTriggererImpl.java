9895a9b026e4580d35e113a731f7ff3e151eee78
Dustin Kut Moy Cheung <dcheung@redhat.com>
2026-09-11 15:52:56 -0400

    Add limited-user generic setting
    This is used to temporarily limit a user from triggering builds.

--- facade/src/main/java/org/jboss/pnc/facade/impl/BuildTriggererImpl.java
+++ facade/src/main/java/org/jboss/pnc/facade/impl/BuildTriggererImpl.java
@@ -361,7 +361,8 @@ public class BuildTriggererImpl implements BuildTriggerer {
 
     private void throwCoreExceptionIfInMaintenanceModeAndNonSystemUser() throws BuildConflictException {
 
-        if (!genericSettingProvider.isCurrentUserAllowedToTriggerBuilds()) {
+        if (genericSettingProvider.isInMaintenanceMode()
+                && !genericSettingProvider.isCurrentUserAllowedToTriggerBuilds()) {
             String reason = genericSettingProvider.getAnnouncementBanner();
 
             if (reason == null) {
@@ -369,6 +370,10 @@ public class BuildTriggererImpl implements BuildTriggerer {
             }
 
             throw new BuildConflictException("PNC is in maintenance mode: " + reason);
+        }
+
+        if (genericSettingProvider.isCurrentUserLimitedBuildUser()) {
+            throw new BuildConflictException("User '" + user.currentUsername() + "' is not allowed to trigger builds.");
         }
     }
 
